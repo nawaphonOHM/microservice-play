95e7ab12a504ae0a2deaa512bfdf7f82bd787244
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-09-14 19:45:36 +0700

    completed first MainController

--- database_per_services/private_table_per_service/src/main/java/nawaphon/microservices/data_per_services/private_table_per_service/controllers/MainController.java
+++ database_per_services/private_table_per_service/src/main/java/nawaphon/microservices/data_per_services/private_table_per_service/controllers/MainController.java
@@ -1,4 +1,34 @@
+package nawaphon.microservices.data_per_services.private_table_per_service.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping("/")
+@ResponseBody
 public class MainController {
+
+    static public class ResponseMessage {
+        private final String message;
+
+        public ResponseMessage(final String message){
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    public MainController() {
+
+    }
+
+
+    @GetMapping("/hello-world")
+    public ResponseMessage firstGetMethod(){
+        return new ResponseMessage("hello world");
+    }
 }
