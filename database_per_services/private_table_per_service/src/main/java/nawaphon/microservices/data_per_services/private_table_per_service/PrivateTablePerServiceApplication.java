8355306403a51003b264bd383550a9ae068a75bf
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-09-14 19:11:35 +0700

    data_per_services/private_table_per_service init

---
+++ database_per_services/private_table_per_service/src/main/java/nawaphon/microservices/data_per_services/private_table_per_service/PrivateTablePerServiceApplication.java
@@ -0,0 +1,13 @@
+package nawaphon.microservices.data_per_services.private_table_per_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateTablePerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateTablePerServiceApplication.class, args);
+	}
+
+}
---
+++ database_per_services/private_table_per_service/src/test/java/nawaphon/microservices/data_per_services/private_table_per_service/PrivateTablePerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nawaphon.microservices.data_per_services.private_table_per_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateTablePerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
