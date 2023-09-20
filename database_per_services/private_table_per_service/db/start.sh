cb197ff7af727641232edc4b870f5a30cbd6267c
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-09-20 21:41:52 +0700

    rm start.sh

--- database_per_services/private_table_per_service/db/start.sh
+++
@@ -1,7 +0,0 @@
-docker run --name 'some-postgres' --publish=5432:5432 --env POSTGRES_PASSWORD=test --detach postgres
-
-docker cp ./initial.sql 'some-postgres':/
-
-sleep 10
-
-docker exec --detach 'some-postgres' psql -U postgres --file /initial.sql
