c093d685d6dc9d4efb0bce2a44a2639bef3c5afc
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-09-20 22:19:42 +0700

    complete OrderRepository

--- database_per_services/private_table_per_service/order_service/src/main/java/nawaphon/microservices/data_per_services/private_table_per_service/repositories/OrderRepository.java
+++ database_per_services/private_table_per_service/order_service/src/main/java/nawaphon/microservices/data_per_services/private_table_per_service/repositories/OrderRepository.java
@@ -1,4 +1,10 @@
 package nawaphon.microservices.data_per_services.private_table_per_service.repositories;
 
-public class OrderRepository {
-}
+import nawaphon.microservices.data_per_services.private_table_per_service.pojo.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, UUID> { }
