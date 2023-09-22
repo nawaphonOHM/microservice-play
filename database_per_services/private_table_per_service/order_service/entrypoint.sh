f32259a628d573034cd9b2cb69ce606f06b689df
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-09-22 09:04:07 +0700

    update entrypoint.sh

--- database_per_services/private_table_per_service/order_service/entrypoint.sh
+++ database_per_services/private_table_per_service/order_service/entrypoint.sh
@@ -11,7 +11,34 @@
 
 main() {
 
-  java -jar /home/order_service/order_service-0.0.1.jar && exit 1
+  ERROR=0
+
+  if [ -z "$DATABASE_URL" ]; then
+    cat >&2 <<-'EOE'
+    Error: DATABASE_URL is required.
+EOE
+  ERROR=1
+  fi
+
+  if [ -z "$DATABASE_USERNAME" ]; then
+    cat >&2 <<-'EOE'
+    Error: DATABASE_USERNAME is required.
+EOE
+  ERROR=1
+  fi
+
+  if [ -z "$DATABASE_PASSWORD" ]; then
+    cat >&2 <<-'EOE'
+    Error: DATABASE_PASSWORD is required.
+EOE
+  ERROR=1
+  fi
+
+  if [ "$ERROR" -eq 1 ]; then
+    exit 1
+  fi
+
+  java -jar /home/order_service/order_service-0.0.1.jar || exit 1
 }
 
 
