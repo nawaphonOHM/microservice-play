5a3be731db3845d471909b774b981f9666279261
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-09-25 22:54:19 +0700

    update shell script for support bridge network in docker

--- database_per_services/private_table_per_service/order_service/entrypoint.sh
+++ database_per_services/private_table_per_service/order_service/entrypoint.sh
@@ -2,34 +2,47 @@
 
 main() {
 
-  ERROR=0
+  export ERROR=0
 
-  if [ -z "$DATABASE_URL" ]; then
-    cat >&2 <<-'EOE'
-    Error: DATABASE_URL is required.
-EOE
-  ERROR=1
-  fi
-
-  if [ -z "$DATABASE_USERNAME" ]; then
-    cat >&2 <<-'EOE'
-    Error: DATABASE_USERNAME is required.
-EOE
-  ERROR=1
-  fi
-
-  if [ -z "$DATABASE_PASSWORD" ]; then
-    cat >&2 <<-'EOE'
-    Error: DATABASE_PASSWORD is required.
-EOE
-  ERROR=1
-  fi
+  mandatoryEnvCheck
 
   if [ "$ERROR" -eq 1 ]; then
-    exit 1
+        exit 1
   fi
 
+  unset ERROR
+
+
+  eth0Ip="$(hostname --all-ip-addresses | awk '{print $1}')"
+
+  echo "My eth0 ip is $eth0Ip"
+
+  export DATABASE_URL="jdbc:postgresql://$eth0Ip:$DATABASE_PORT/postgres"
+
   java -jar /home/order_service/order_service-0.0.1.jar || exit 1
+}
+
+mandatoryEnvCheck() {
+  if [ -z "$DATABASE_PORT" ]; then
+      cat >&2 <<-'EOE'
+      Error: DATABASE_PORT is required.
+EOE
+    ERROR=1
+    fi
+
+    if [ -z "$DATABASE_USERNAME" ]; then
+      cat >&2 <<-'EOE'
+      Error: DATABASE_USERNAME is required.
+EOE
+    ERROR=1
+    fi
+
+    if [ -z "$DATABASE_PASSWORD" ]; then
+      cat >&2 <<-'EOE'
+      Error: DATABASE_PASSWORD is required.
+EOE
+    ERROR=1
+    fi
 }
 
 
