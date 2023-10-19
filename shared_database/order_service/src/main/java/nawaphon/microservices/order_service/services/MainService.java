bfc7a36abbf2fbddf26222b4973b6c668cfa5a6d
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-10-19 22:45:06 +0700

    update firstService service method to be able to query data

--- shared_database/order_service/src/main/java/nawaphon/microservices/order_service/services/MainService.java
+++ shared_database/order_service/src/main/java/nawaphon/microservices/order_service/services/MainService.java
@@ -1,14 +1,23 @@
 package nawaphon.microservices.order_service.services;
 
+import nawaphon.microservice.pojo.Customer;
 import nawaphon.microservice.pojo.Order;
 import nawaphon.microservice.pojo.ResponseMessage;
 import nawaphon.microservices.order_service.repositories.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class MainService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainService.class);
 
     private final OrderRepository orderRepository;
 
@@ -17,9 +26,27 @@ public class MainService {
     }
 
 
-    public ResponseMessage<List<Order>> firstService() {
-        final List<Order> results = this.orderRepository.findAll();
+    public ResponseMessage<List<Order>> getOrderByCriteria(final Map<String, String> params) {
+        final Order probe = new Order();
 
-        return new ResponseMessage<>(200, "Done", results);
+        probe.setCustomerId(new Customer());
+
+        params.forEach((key, value) -> {
+            if (key.equalsIgnoreCase("id")) {
+                logger.debug("There is id = {}", value);
+                probe.setId(UUID.fromString(value));
+            } else if (key.equalsIgnoreCase("customerId")) {
+                logger.debug("There is customerId = {}", value);
+                probe.getCustomerId().setId(UUID.fromString(value));
+            } else if (key.equalsIgnoreCase("status")) {
+                logger.debug("There is status = {}", value);
+                probe.setStatus(Boolean.parseBoolean(value));
+            } else if (key.equalsIgnoreCase("total")) {
+                logger.debug("There is total = {}", value);
+                probe.setTotal(new BigDecimal(value));
+            }
+        });
+
+        return orderRepository.findBy(Example.of(probe), (query) -> new ResponseMessage<>(200, "Done", query.all()));
     }
 }
