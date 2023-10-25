c2c9fefcd050190dab3ea984d20ae93ffa856970
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-10-25 21:56:54 +0700

    update common package

--- database_per_services/private_table_per_service/customer_service/src/main/java/nawaphon/microservices/customer_service/CustomerServiceApplication.java
+++ database_per_services/private_table_per_service/customer_service/src/main/java/nawaphon/microservices/customer_service/CustomerServiceApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("nawaphon.microservice.pojo")
+@EntityScan("nawaphon.microservice.main.common.pojo")
 public class CustomerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
 
 }
