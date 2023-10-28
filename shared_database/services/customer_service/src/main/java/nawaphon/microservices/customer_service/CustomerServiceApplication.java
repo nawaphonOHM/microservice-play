9817c17074074563001d963bc034126ceb3c2629
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-10-29 00:53:17 +0700

    configure basePackage to scan

--- shared_database/services/customer_service/src/main/java/nawaphon/microservices/customer_service/CustomerServiceApplication.java
+++ shared_database/services/customer_service/src/main/java/nawaphon/microservices/customer_service/CustomerServiceApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"nawaphon.microservices.customer_service", "nawaphon.microservice.shared_database.common.repositories"})
 @EntityScan("nawaphon.microservice.pojo")
 public class CustomerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
 
 }
--- shared_database/services/order_service/src/main/java/nawaphon/microservices/order_service/OrderServiceApplication.java
+++ shared_database/services/order_service/src/main/java/nawaphon/microservices/order_service/OrderServiceApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"nawaphon.microservices.order_service", "nawaphon.microservice.shared_database.common.repositories"})
 @EntityScan("nawaphon.microservice.pojo")
 public class OrderServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
 
 }
