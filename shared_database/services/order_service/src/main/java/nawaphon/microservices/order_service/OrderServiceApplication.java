1549c6dde8db12af4c18c2b8d4c2312ca8c4f277
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-10-29 20:31:38 +0700

    update package to scanning

--- shared_database/services/order_service/src/main/java/nawaphon/microservices/order_service/OrderServiceApplication.java
+++ shared_database/services/order_service/src/main/java/nawaphon/microservices/order_service/OrderServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = {"nawaphon.microservices.order_service", "nawaphon.microservice.shared_database.common.repositories"})
-@EntityScan("nawaphon.microservice.pojo")
+@SpringBootApplication(scanBasePackages = {"nawaphon.microservice.shared_database.common.repositories"})
+@EntityScan(basePackages = {"nawaphon.microservice.pojo"})
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
