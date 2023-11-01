027f8061788f6d7cc052b3649fdf3d924c7d96c2
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-11-01 21:16:34 +0700

    complete CustomerNotFoundExceptionAdvice

--- shared_database/services/customer_service/src/main/java/nawaphon/microservices/customer_service/exception_advices/CustomerNotFoundExceptionAdvice.java
+++ shared_database/services/customer_service/src/main/java/nawaphon/microservices/customer_service/exception_advices/CustomerNotFoundExceptionAdvice.java
@@ -1,4 +1,19 @@
 package nawaphon.microservices.customer_service.exception_advices;
 
+import nawaphon.microservice.main.common.pojo.ResponseMessage;
+import nawaphon.microservices.customer_service.exceptions.CustomerNotFoundException;
+import nawaphon.microservices.customer_service.exceptions.FailToSaveCustomerException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class CustomerNotFoundExceptionAdvice {
+
+    @ExceptionHandler({CustomerNotFoundException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseMessage<String> buildMessage(final FailToSaveCustomerException exception) {
+        return new ResponseMessage<>(HttpStatus.BAD_REQUEST.value(), exception.getStatusMessage(), exception.getDetailMessage());
+    }
 }
