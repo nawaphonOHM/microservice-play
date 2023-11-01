8d0cc736f4bdbe2a72a26d26f2f4589b3f0bf3be
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-11-01 21:35:11 +0700

    add UpdateNewCreditFailedExceptionAdvice

---
+++ shared_database/services/customer_service/src/main/java/nawaphon/microservices/customer_service/exception_advices/UpdateNewCreditFailedExceptionAdvice.java
@@ -0,0 +1,18 @@
+package nawaphon.microservices.customer_service.exception_advices;
+
+import nawaphon.microservice.main.common.pojo.ResponseMessage;
+import nawaphon.microservices.customer_service.exceptions.UpdateNewCreditFailedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UpdateNewCreditFailedExceptionAdvice {
+
+    @ExceptionHandler({UpdateNewCreditFailedException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseMessage<String> buildMessage() {
+        return new ResponseMessage<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), HttpStatus.INTERNAL_SERVER_ERROR.toString());
+    }
+}
