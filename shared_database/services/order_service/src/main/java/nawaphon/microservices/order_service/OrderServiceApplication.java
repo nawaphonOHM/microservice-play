d5ca6f4141ea321639f656dd6a0d96347a1b6202
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-11-02 22:25:43 +0700

    update @SpringBootApplication config and @EntityScan

--- shared_database/services/order_service/src/main/java/nawaphon/microservices/order_service/OrderServiceApplication.java
+++ shared_database/services/order_service/src/main/java/nawaphon/microservices/order_service/OrderServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = {"nawaphon.microservice.shared_database.common.repositories"})
-@EntityScan(basePackages = {"nawaphon.microservice.pojo"})
+@SpringBootApplication(scanBasePackages = {"nawaphon.microservice.shared_database.common.exception_advices"})
+@EntityScan(basePackages = {"nawaphon.microservice.shared_database.common.repositories", "nawaphon.microservice.pojo"})
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
