1665ac3a84041c96a8a9ff056a4a5ee50c69d922
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-11-03 23:07:48 +0700

    complete addOrder service

--- shared_database/services/order_service/src/main/java/nawaphon/microservices/order_service/services/RequiredTransactionalService.java
+++ shared_database/services/order_service/src/main/java/nawaphon/microservices/order_service/services/RequiredTransactionalService.java
@@ -1,13 +1,17 @@
 package nawaphon.microservices.order_service.services;
 
+import nawaphon.microservice.main.common.pojo.Customer;
 import nawaphon.microservice.main.common.pojo.Order;
-import nawaphon.microservice.shared_database.common.enums.OrderStatus;
-import nawaphon.microservice.shared_database.common.pojo.OrderStatusEnvelop;
+import nawaphon.microservice.shared_database.common.exceptions.CustomerNotFoundException;
+import nawaphon.microservice.shared_database.common.repositories.CustomerRepository;
 import nawaphon.microservice.shared_database.common.repositories.OrderRepository;
+import nawaphon.microservices.order_service.exceptions.InsufficientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
 @Transactional
@@ -16,11 +20,25 @@ public class RequiredTransactionalService {
 
     private final OrderRepository orderRepository;
 
-    public RequiredTransactionalService(final OrderRepository orderRepository) {
+    private final CustomerRepository customerRepository;
+
+    public RequiredTransactionalService(final OrderRepository orderRepository, final CustomerRepository customerRepository) {
         this.orderRepository = orderRepository;
+        this.customerRepository = customerRepository;
     }
 
-    public OrderStatusEnvelop addOrder(Order order) {
-        return new OrderStatusEnvelop(orderRepository.save(order), OrderStatus.PENDING);
+    public boolean addOrder(Order order) {
+        final Customer customer = this.customerRepository.findById(order.getCustomerId().getId()).orElseThrow(CustomerNotFoundException::new);
+
+        customer.setCreditLimit(customer.getCreditLimit().subtract(new BigDecimal(String.valueOf(order.getTotal()))));
+
+        if (customer.getCreditLimit().compareTo(BigDecimal.ZERO) < 0) {
+            throw new InsufficientException();
+        }
+
+        orderRepository.save(order);
+
+        return true;
+
     }
 }
