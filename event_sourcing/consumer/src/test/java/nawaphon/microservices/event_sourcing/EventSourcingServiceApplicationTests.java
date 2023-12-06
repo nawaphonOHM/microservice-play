49e6cd134a3a27360544b341ad277e01bf9ef4ae
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-12-06 23:50:40 +0700

    add event-sourcing/consumer project

---
+++ event_sourcing/consumer/src/test/java/nawaphon/microservices/event_sourcing/EventSourcingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nawaphon.microservices.event_sourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
