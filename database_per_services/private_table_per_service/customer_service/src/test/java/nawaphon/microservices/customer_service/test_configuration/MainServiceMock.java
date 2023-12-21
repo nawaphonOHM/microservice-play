8b357e80fac8b27977a91fc3753e3148a8e682a0
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-12-21 20:47:45 +0700

    complete MainService mocking

--- database_per_services/private_table_per_service/customer_service/src/test/java/nawaphon/microservices/customer_service/test_configuration/MainServiceMock.java
+++ database_per_services/private_table_per_service/customer_service/src/test/java/nawaphon/microservices/customer_service/test_configuration/MainServiceMock.java
@@ -1,15 +1,33 @@
 package nawaphon.microservices.customer_service.test_configuration;
 
+import nawaphon.microservices.customer_service.pojo.Customer;
+import nawaphon.microservices.customer_service.repositories.CustomerRepository;
 import nawaphon.microservices.customer_service.services.MainService;
 import nawaphon.microservices.customer_service.services.MainServiceImpl;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @TestConfiguration
 public class MainServiceMock {
 
     @Bean
     public MainService mainService() {
+
+        final Customer customerTest = new Customer();
+
+        customerTest.setCreditLimit(new BigDecimal(1000));
+
+        final List<Customer> customers = List.of(customerTest);
+
+        final CustomerRepository customerRepository = Mockito.mock(CustomerRepository.class);
+
+        Mockito.when(customerRepository.findAll()).thenReturn(customers);
+
+
         return new MainServiceImpl(customerRepository);
     }
 
