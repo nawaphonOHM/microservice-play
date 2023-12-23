3247dda0c71cf7ee0aec420aac4aaf58a4f8602c
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2023-12-23 19:48:04 +0700

    add @BeforeEach

--- database_per_services/private_table_per_service/customer_service/src/test/java/nawaphon/microservices/customer_service/services/MainServiceImplTest.java
+++ database_per_services/private_table_per_service/customer_service/src/test/java/nawaphon/microservices/customer_service/services/MainServiceImplTest.java
@@ -1,11 +1,15 @@
 package nawaphon.microservices.customer_service.services;
 
+import nawaphon.microservices.customer_service.pojo.Customer;
+import nawaphon.microservices.customer_service.pojo.ResponseMessage;
 import nawaphon.microservices.customer_service.test_configuration.MainServiceMock;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {MainServiceMock.class})
@@ -15,8 +19,10 @@ class MainServiceImplTest {
     @Autowired
     private MainService mainService;
 
+    private ResponseMessage<List<Customer>> testResult;
 
-    @Test
-    void firstServiceTest() {
+    @BeforeEach
+    void setUp() {
+        testResult = mainService.firstService();
     }
 }
