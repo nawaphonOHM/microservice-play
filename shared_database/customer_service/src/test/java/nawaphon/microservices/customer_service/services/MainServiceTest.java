c982f19c70eee18b6e8189bb52f318a0f65752ca
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-04 00:28:00 +0700

    Enhance MainServiceTest with add and update functions testing
    Added two new test methods in the MainServiceTest class to verify the correct behavior of customer addition and credit update. Tests involve ensuring that the interaction with the CustomerRepository, such as save and findById methods, are correctly invoked with expected parameters. This augments our test coverage and provides verification for crucial service functions.

--- shared_database/customer_service/src/test/java/nawaphon/microservices/customer_service/services/MainServiceTest.java
+++ shared_database/customer_service/src/test/java/nawaphon/microservices/customer_service/services/MainServiceTest.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @ExtendWith(SpringExtension.class)
@@ -39,11 +41,16 @@ public class MainServiceTest {
 
     @Test
     void testAbleToAddNewCustomer() {
+        final UUID id = UUID.randomUUID();
+
         final Customer customer = new Customer();
+
+        customer.setId(id);
 
         mainService.addNewCustomer(customer);
 
-        BDDMockito.verify(customerRepository, BDDMockito.times(1)).save(BDDMockito.any());
+        BDDMockito.verify(customerRepository, BDDMockito.times(1))
+                .save(BDDMockito.argThat((var1) -> var1.getId() == id));
     }
 
 
@@ -54,6 +61,28 @@ public class MainServiceTest {
         mainService.removeCustomer(id);
 
         BDDMockito.verify(customerRepository, BDDMockito.times(1)).deleteById(BDDMockito.any());
+    }
+
+    @Test
+    void testAbleToUpdateCredit() {
+        final UUID id = UUID.randomUUID();
+        final BigDecimal newCredit = BigDecimal.TEN;
+
+        final Customer mockCustomer = BDDMockito.mock(Customer.class);
+
+        final Optional<Customer> optionalCustomer = Optional.of(mockCustomer);
+
+        BDDMockito.given(mockCustomer.getId()).willReturn(id);
+
+        BDDMockito.given(customerRepository.findById(BDDMockito.any())).willReturn(optionalCustomer);
+
+        mainService.updateUserCredit(id, newCredit);
+
+        BDDMockito.verify(customerRepository, BDDMockito.times(1)).findById(
+                BDDMockito.any());
+
+        BDDMockito.verify(customerRepository, BDDMockito.times(1))
+                .save(BDDMockito.argThat((var1) -> var1.getId() == id));
     }
 
 }
