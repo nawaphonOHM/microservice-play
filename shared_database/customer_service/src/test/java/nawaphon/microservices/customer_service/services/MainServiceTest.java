2ad87e8796103e427aa902f6b8839b33e50a4a09
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-04 00:13:17 +0700

    Add customer criteria test in MainServiceTest
    Extended the MainServiceTest class to include testing for the getCustomerByCriteria method. Mockito is used to simulate interactions with the CustomerRepository and verify it's called correctly. This enhancement improves the reliability of the service when retrieving customer criteria.

--- shared_database/customer_service/src/test/java/nawaphon/microservices/customer_service/services/MainServiceTest.java
+++ shared_database/customer_service/src/test/java/nawaphon/microservices/customer_service/services/MainServiceTest.java
@@ -2,10 +2,15 @@ package nawaphon.microservices.customer_service.services;
 
 import nawaphon.microservices.customer_service.repositories.CustomerRepository;
 import nawaphon.microservices.customer_service.test_configurations.MainServiceMock;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = MainServiceMock.class)
@@ -16,5 +21,18 @@ public class MainServiceTest {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+
+    @Test
+    void testAbleToGetCustomerByCriteria() {
+        final Map<String, String> params = new HashMap<>();
+
+        mainService.getCustomerByCriteria(params);
+
+        BDDMockito.verify(customerRepository, BDDMockito.times(1)).findBy(
+                BDDMockito.any(),
+                BDDMockito.any()
+        );
+    }
 
 }
