0786b65298a4c1cdab5e67b3b9ff219890a1ddba
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-04 14:03:39 +0700

    Add CustomerRepositoryTest class
    Implemented the CustomerRepositoryTest class for unit test coverage. The class includes setup and test methods to verify data retrieval from the CustomerRepository, ensuring accurate database interaction and consistent application behavior.

---
+++ shared_database/customer_service/src/test/java/nawaphon/microservices/customer_service/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,60 @@
+package nawaphon.microservices.customer_service.repositories;
+
+import nawaphon.microservices.customer_service.pojo.Customer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Example;
+import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ActiveProfiles("test")
+public class CustomerRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    private UUID uuid1;
+
+
+    @BeforeEach
+    public void setUp() {
+        final Customer customer = new Customer();
+
+        customer.setCreditLimit(BigDecimal.valueOf(1000));
+
+        uuid1 = entityManager.persist(customer).getId();
+
+    }
+
+
+    @Test
+    void ableFindByProperlyWhenSetOnlyId() {
+        final Customer customer = new Customer();
+
+        customer.setId(uuid1);
+
+        final List<Customer> foundCustomer = customerRepository.findBy(Example.of(customer),
+                FluentQuery.FetchableFluentQuery::all);
+
+        Assertions.assertEquals(1, foundCustomer.size());
+        Assertions.assertEquals(uuid1, foundCustomer.get(0).getId());
+        Assertions.assertEquals(BigDecimal.valueOf(1000), foundCustomer.get(0).getCreditLimit());
+
+    }
+
+}
