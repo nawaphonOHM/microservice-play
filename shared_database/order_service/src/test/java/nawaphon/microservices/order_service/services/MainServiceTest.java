474d1939cfc23c77732920989339a408d87493be
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-18 20:08:27 +0700

    Add test for getOrderByCriteria in MainService
    Added a new test method, testGetOrderByCriteriaCallRightService, in the MainServiceTest class to verify if the getOrderByCriteria method calls the right service. Also, introduced new imports to facilitate this test scenario.

--- shared_database/order_service/src/test/java/nawaphon/microservices/order_service/services/MainServiceTest.java
+++ shared_database/order_service/src/test/java/nawaphon/microservices/order_service/services/MainServiceTest.java
@@ -1,10 +1,15 @@
 package nawaphon.microservices.order_service.services;
 
 import nawaphon.microservices.order_service.test_configurations.MainServiceMock;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = MainServiceMock.class)
@@ -18,6 +23,15 @@ public class MainServiceTest {
 
     @Autowired
     private MainService mainService;
+
+
+    @Test
+    public void testGetOrderByCriteriaCallRightService() {
+        final Map<String, String> map = new HashMap<>();
+        mainService.getOrderByCriteria(map);
+
+        Mockito.verify(nonRequiredTransactionalService, Mockito.times(1)).getOrderByCriteria(Mockito.same(map));
+    }
 
 
 }
