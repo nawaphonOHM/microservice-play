bd6a4bfdfad85fae0e109d661543b7d8d9b479e4
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-18 22:27:58 +0700

    Add NonRequiredTransactionalService test class
    Added a new test class, NonRequiredTransactionalServiceTest.java, for the Order Service. This class is designed to provide comprehensive unit tests on the NonRequiredTransactionalService, particularly on the getOrderByCriteria method. It includes mock instances of OrderRepository and NonRequiredTransactionalService to ensure accurate and isolated testing results.

---
+++ shared_database/order_service/src/test/java/nawaphon/microservices/order_service/services/NonRequiredTransactionalServiceTest.java
@@ -0,0 +1,40 @@
+package nawaphon.microservices.order_service.services;
+
+import nawaphon.microservices.order_service.repositories.OrderRepository;
+import nawaphon.microservices.order_service.test_configurations.MainServiceMock2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = MainServiceMock2.class)
+public class NonRequiredTransactionalServiceTest {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private NonRequiredTransactionalService nonRequiredTransactionalService;
+
+
+    @Test
+    public void testGetOrderByCriteriaMethod() {
+        final Map<String, String> map = new HashMap<>();
+
+        final UUID uuidMock = UUID.randomUUID();
+
+        map.put("id", uuidMock.toString());
+
+        nonRequiredTransactionalService.getOrderByCriteria(map);
+
+        Mockito.verify(orderRepository, Mockito.times(1)).findBy(Mockito.argThat((probe) -> probe.getProbe().getId().toString().equals(uuidMock.toString())), Mockito.any());
+    }
+
+}
