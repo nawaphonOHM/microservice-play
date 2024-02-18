948c90542d8ed005c68167a17532f8e68c8e8aee
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-18 20:00:42 +0700

    Update test configuration to include MainService mock
    MainService mock has been added to the test configuration for better control during testing. This creates a MainService bean, instantiated with RequiredTransactionalServices. This adjustment simplifies dependency management across tests.

--- shared_database/order_service/src/test/java/nawaphon/microservices/order_service/test_configurations/MainServiceMock.java
+++ shared_database/order_service/src/test/java/nawaphon/microservices/order_service/test_configurations/MainServiceMock.java
@@ -1,8 +1,10 @@
 package nawaphon.microservices.order_service.test_configurations;
 
+import nawaphon.microservices.order_service.repositories.OrderRepository;
 import nawaphon.microservices.order_service.services.MainService;
 import nawaphon.microservices.order_service.services.NonRequiredTransactionalService;
 import nawaphon.microservices.order_service.services.RequiredTransactionalService;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
@@ -13,5 +15,10 @@ public class MainServiceMock {
     public MainService mainService(final RequiredTransactionalService requiredTransactionalService, final NonRequiredTransactionalService nonRequiredTransactionalService) {
 
         return new MainService(requiredTransactionalService, nonRequiredTransactionalService);
+    }
+
+    @Bean
+    public RequiredTransactionalService requiredTransactionalService(final OrderRepository orderRepository) {
+        return Mockito.mock(RequiredTransactionalService.class);
     }
 }
