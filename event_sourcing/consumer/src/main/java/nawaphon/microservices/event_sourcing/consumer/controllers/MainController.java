72176737f6d927bc96b8fe134e55dffc0b0abea9
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-20 22:33:46 +0700

    Incorporate Kafka listener and request handling in MainController
    Introduced the Kafka listener annotation and set up request handling method in MainController of event_sourcing consumer. This enhancement will listen to Kafka topics and log received messages, improving event sourcing capabilities.

--- event_sourcing/consumer/src/main/java/nawaphon/microservices/event_sourcing/consumer/controllers/MainController.java
+++ event_sourcing/consumer/src/main/java/nawaphon/microservices/event_sourcing/consumer/controllers/MainController.java
@@ -1,11 +1,20 @@
 package nawaphon.microservices.event_sourcing.consumer.controllers;
 
+import nawaphon.microservices.event_sourcing.consumer.pojo.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class MainController {
 
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @KafkaListener(topics = "Greeting")
+    public void sendEvent(@RequestBody final Message message) {
+        logger.info("I've received message: {}", message.getMessage());
+
+    }
 }
