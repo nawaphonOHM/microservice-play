6d415e2098d749ddb4dfac61901ed84752293738
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-20 22:33:31 +0700

    Add MainController to event_sourcing consumer
    Added a new Java class, MainController, to the event_sourcing consumer module. The class is responsible for controlling the main application route, and includes the implementation of the SLF4J logging facade for logging events.

---
+++ event_sourcing/consumer/src/main/java/nawaphon/microservices/event_sourcing/consumer/controllers/MainController.java
@@ -0,0 +1,11 @@
+package nawaphon.microservices.event_sourcing.consumer.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+}
