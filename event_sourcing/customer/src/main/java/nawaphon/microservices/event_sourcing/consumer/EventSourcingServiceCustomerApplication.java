a9d7a67329c48e7bea25eacb0eda808ac878dfe6
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-24 13:27:33 +0700

    Enable KafkaStreams in Customer Application
    Enabled KafkaStreams by adding the @EnableKafkaStreams annotation in the EventSourcingServiceCustomerApplication. This change allows the application to handle Kafka stream processing, thus improving data processing capabilities.

--- event_sourcing/customer/src/main/java/nawaphon/microservices/event_sourcing/consumer/EventSourcingServiceCustomerApplication.java
+++ event_sourcing/customer/src/main/java/nawaphon/microservices/event_sourcing/consumer/EventSourcingServiceCustomerApplication.java
@@ -2,8 +2,10 @@ package nawaphon.microservices.event_sourcing.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
+@EnableKafkaStreams
 public class EventSourcingServiceCustomerApplication {
 
     public static void main(String[] args) {
