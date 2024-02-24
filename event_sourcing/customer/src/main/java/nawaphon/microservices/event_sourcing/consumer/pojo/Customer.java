2bd6125a676cd29b0b45c05fb5d767bd068b95e7
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-24 12:58:15 +0700

    Add Customer POJO in the consumer service
    Created a new `Customer` class under the consumer service of our event sourcing project. This POJO includes properties for `id` and `creditLimit`, giving structure to the customer’s essential information which will further be used in event sourcing interactions.

---
+++ event_sourcing/customer/src/main/java/nawaphon/microservices/event_sourcing/consumer/pojo/Customer.java
@@ -0,0 +1,35 @@
+package nawaphon.microservices.event_sourcing.consumer.pojo;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Customer {
+
+    private UUID id;
+
+    private BigDecimal creditLimit;
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public BigDecimal getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(BigDecimal creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", creditLimit=" + creditLimit +
+                '}';
+    }
+}
