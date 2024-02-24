7bb2d35a6328a474582e44dc811607b999e2e03a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-24 17:34:14 +0700

    Update MainService in event_sourcing to facilitate customer search by ID
    Expanded the functionality of the MainService class in the 'event_sourcing' directory to add a 'searchCustomerById' method for retrieving individual customers by their IDs. The method gets a read-only instance of a key-value store from a Kafka stream, and accesses the required customer data. Results are then processed through an exception handling boilerplate for orderly output.

--- event_sourcing/customer/src/main/java/nawaphon/microservices/event_sourcing/consumer/services/MainService.java
+++ event_sourcing/customer/src/main/java/nawaphon/microservices/event_sourcing/consumer/services/MainService.java
@@ -4,8 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nawaphon.microservices.event_sourcing.consumer.exceptions.UnclassifiedException;
 import nawaphon.microservices.event_sourcing.consumer.pojo.Customer;
+import nawaphon.microservices.event_sourcing.consumer.pojo.CustomerId;
 import nawaphon.microservices.event_sourcing.consumer.pojo.ResponseMessage;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StoreQueryParameters;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
@@ -21,11 +29,13 @@ public class MainService {
 
     private final ObjectMapper objectMapper;
 
-    public MainService(final KafkaTemplate<UUID, String> kafkaTemplate, final ObjectMapper objectMapper) {
+    private final StreamsBuilderFactoryBean streamsBuilderFactoryBean;
+
+    public MainService(final KafkaTemplate<UUID, String> kafkaTemplate, final ObjectMapper objectMapper, final StreamsBuilderFactoryBean streamsBuilderFactoryBean) {
         this.kafkaTemplate = kafkaTemplate;
         this.objectMapper = objectMapper;
+        this.streamsBuilderFactoryBean = streamsBuilderFactoryBean;
     }
-
 
     public ResponseMessage<Customer> addCustomer(final Customer customer) {
 
@@ -48,7 +58,22 @@ public class MainService {
             }
         }
 
-
         return new ResponseMessage<>(HttpStatus.OK.value(), HttpStatus.OK.toString(), customer);
+    }
+
+    public ResponseMessage<Customer> searchCustomerById(final CustomerId customerId) {
+        final KafkaStreams kafkaStreams = streamsBuilderFactoryBean.getKafkaStreams();
+        assert kafkaStreams != null;
+        final ReadOnlyKeyValueStore<UUID, String> readOnlyKeyValueStore = kafkaStreams.store(
+                StoreQueryParameters.fromNameAndType("orderCustomer", QueryableStoreTypes.keyValueStore())
+        );
+
+        try {
+            return new ResponseMessage<>(HttpStatus.OK.value(), HttpStatus.OK.toString(),
+                    objectMapper.readValue(readOnlyKeyValueStore.get(customerId.getId()), Customer.class));
+
+        } catch (JsonProcessingException e) {
+            throw new UnclassifiedException("There is error while deserializing Customer Object", e);
+        }
     }
 }
