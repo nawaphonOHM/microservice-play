edfe1427e2ed660dc1c9e3c8b067e787081bb9a2
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-24 19:54:40 +0700

    Improve Kafka setup by adding Serdes to OrderCustomerStreamComponent
    Placing Kafka properties directly in the OrderCustomerStreamComponent simplifies the setup process. This enhances maintainability by reducing redundancy and centralizing Kafka configurations.

--- event_sourcing/customer/src/main/java/nawaphon/microservices/event_sourcing/consumer/components/OrderCustomerStreamComponent.java
+++ event_sourcing/customer/src/main/java/nawaphon/microservices/event_sourcing/consumer/components/OrderCustomerStreamComponent.java
@@ -1,11 +1,15 @@
 package nawaphon.microservices.event_sourcing.consumer.components;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class OrderCustomerStreamComponent {
@@ -13,6 +17,11 @@ public class OrderCustomerStreamComponent {
 
     @Autowired
     public void setUp(final StreamsBuilder streamsBuilder) {
-        streamsBuilder.stream("orderCustomer", Consumed.with(Serdes.UUID(), Serdes.String())).toTable(Materialized.as("orderCustomer"));
+
+        streamsBuilder.stream("orderCustomer", Consumed.with(Serdes.UUID(), Serdes.String())).toTable(
+                Materialized.<UUID, String, KeyValueStore<Bytes, byte[]>>
+                        as("orderCustomer").withKeySerde(Serdes.UUID()).withValueSerde(Serdes.String())
+        );
+
     }
 }
