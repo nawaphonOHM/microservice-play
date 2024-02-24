967706dde90f4b29928cc468f6c13eda0ccc4cde
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-24 13:24:14 +0700

    Update MainController with new CRUD methods
    The MainController class has been significantly refactored. It now imports new dependencies and includes several new methods to post a new customer, get a customer by criteria, update customer credit, and remove a customer. Moreover, the structure of the controller has been modified to match the design of a RESTful API controller.

--- event_sourcing/customer/src/main/java/nawaphon/microservices/event_sourcing/consumer/controllers/MainController.java
+++ event_sourcing/customer/src/main/java/nawaphon/microservices/event_sourcing/consumer/controllers/MainController.java
@@ -1,20 +1,44 @@
 package nawaphon.microservices.event_sourcing.consumer.controllers;
 
-import nawaphon.microservices.event_sourcing.consumer.pojo.Message;
+import nawaphon.microservices.event_sourcing.consumer.pojo.Customer;
+import nawaphon.microservices.event_sourcing.consumer.pojo.CustomerId;
+import nawaphon.microservices.event_sourcing.consumer.pojo.ResponseMessage;
+import nawaphon.microservices.event_sourcing.consumer.services.MainService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
 public class MainController {
 
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
-    @KafkaListener(topics = "Greeting", groupId = "group00")
-    public void sendEvent(@RequestBody final Message message) {
-        logger.info("I've received message: {}", message.getMessage());
+    private final MainService mainService;
 
+    public MainController(final MainService mainService) {
+        this.mainService = mainService;
+    }
+
+    @PostMapping("/post-customer")
+    public ResponseMessage<?> postNewCustomer(@RequestBody final Customer newCustomer) {
+        return mainService.addCustomer(newCustomer);
+    }
+
+    @GetMapping("/get-customer-by-criteria")
+    public ResponseMessage<?> getCustomerByCriteria(@RequestParam final Map<String, String> params) {
+        throw new RuntimeException("Not Implemented");
+    }
+
+    @PatchMapping("/update-customer-credit/{customer-uuid}")
+    public ResponseMessage<?> patchCustomerCredit(@PathVariable("customer-uuid") final UUID customerUUID, @RequestBody final CustomerId credit) {
+        throw new RuntimeException("Not Implemented");
+    }
+
+    @DeleteMapping("delete-customer-credit/{customer-uuid}")
+    public ResponseMessage<?> removeCustomer(@PathVariable("customer-uuid") final UUID customerUUID) {
+        throw new RuntimeException("Not Implemented");
     }
 }
