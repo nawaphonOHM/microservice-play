d90379fea64796958bcb69ee340f496d5c2d3cc8
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-02-24 19:27:44 +0700

    Add Serdes to OrderCustomerStreamComponent in Kafka setup
    Kafka properties previously defined in application.properties are now handled directly within the OrderCustomerStreamComponent class. This update reduces redundancy and centralizes Kafka setup, creating a more streamlined setup process.

--- event_sourcing/customer/src/main/java/nawaphon/microservices/event_sourcing/consumer/components/OrderCustomerStreamComponent.java
+++ event_sourcing/customer/src/main/java/nawaphon/microservices/event_sourcing/consumer/components/OrderCustomerStreamComponent.java
@@ -1,6 +1,8 @@
 package nawaphon.microservices.event_sourcing.consumer.components;
 
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,6 @@ public class OrderCustomerStreamComponent {
 
     @Autowired
     public void setUp(final StreamsBuilder streamsBuilder) {
-        streamsBuilder.stream("orderCustomer").toTable(Materialized.as("orderCustomer"));
+        streamsBuilder.stream("orderCustomer", Consumed.with(Serdes.UUID(), Serdes.String())).toTable(Materialized.as("orderCustomer"));
     }
 }
