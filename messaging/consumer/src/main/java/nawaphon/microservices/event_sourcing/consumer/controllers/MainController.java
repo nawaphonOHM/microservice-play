c0d16cd849e8292682298a3e94fa2ab54d30c288
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-03-09 23:26:30 +0700

    update imports

--- messaging/consumer/src/main/java/nawaphon/microservices/event_sourcing/consumer/controllers/MainController.java
+++ messaging/consumer/src/main/java/nawaphon/microservices/event_sourcing/consumer/controllers/MainController.java
@@ -1,11 +1,21 @@
 package nawaphon.microservices.event_sourcing.consumer.controllers;
 
+import nawaphon.microservices.event_sourcing.consumer.pojo.Customer;
+import nawaphon.microservices.event_sourcing.consumer.pojo.CustomerDetail;
 import nawaphon.microservices.event_sourcing.consumer.pojo.Message;
+import nawaphon.microservices.event_sourcing.consumer.pojo.ResponseMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @Controller
 public class MainController {
