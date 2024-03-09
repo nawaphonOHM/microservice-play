6e75003ae625bce167301f8175da25935d2e9988
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-03-09 18:22:24 +0700

    Add 'detailsId' field to 'Customer' class
    A new field 'detailsId' has been added to 'Customer' class, along with its getter method. The constructor has also been modified to initialize the 'detailsId'. This will help to link the 'Customer' object with the corresponding 'details' object for better object structure and future queries.

--- messaging/consumer/src/main/java/nawaphon/microservices/event_sourcing/consumer/pojo/Customer.java
+++ messaging/consumer/src/main/java/nawaphon/microservices/event_sourcing/consumer/pojo/Customer.java
@@ -6,12 +6,19 @@ public class Customer {
 
     private final UUID id;
 
+    private final UUID detailsId;
 
-    public Customer(final UUID id) {
+
+    public Customer(final UUID id, final UUID detailsId) {
         this.id = id;
+        this.detailsId = detailsId;
     }
 
     public UUID getId() {
         return id;
+    }
+
+    public UUID getDetailsId() {
+        return detailsId;
     }
 }
