1790e608159041c2ea91b24053f3e3813a9ce125
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-03-10 21:26:27 +0700

    Import Jackson libraries into MainController
    Two libraries, `com.fasterxml.jackson.core.JsonProcessingException` and `com.fasterxml

--- messaging/consumer/src/main/java/nawaphon/microservices/event_sourcing/consumer/controllers/MainController.java
+++ messaging/consumer/src/main/java/nawaphon/microservices/event_sourcing/consumer/controllers/MainController.java
@@ -1,5 +1,7 @@
 package nawaphon.microservices.event_sourcing.consumer.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nawaphon.microservices.event_sourcing.consumer.pojo.Customer;
 import nawaphon.microservices.event_sourcing.consumer.pojo.CustomerDetail;
 import nawaphon.microservices.event_sourcing.consumer.pojo.Message;
