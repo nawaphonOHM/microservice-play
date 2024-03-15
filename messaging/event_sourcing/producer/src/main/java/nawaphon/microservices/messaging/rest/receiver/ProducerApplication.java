2b6359dfcc99f0dcff207b42c842073b2431819a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-03-15 22:38:34 +0700

    Rename EventSourcingServiceProducerApplication to ProducerApplication
    Renamed the EventSourcingServiceProducerApplication Java class to ProducerApplication for simplicity and clarity. This name change has also been updated in the SpringApplication.run method to maintain code consistency.

--- messaging/event_sourcing/producer/src/main/java/nawaphon/microservices/messaging/rest/receiver/ProducerApplication.java
+++ messaging/event_sourcing/producer/src/main/java/nawaphon/microservices/messaging/rest/receiver/ProducerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventSourcingServiceProducerApplication {
+public class ProducerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EventSourcingServiceProducerApplication.class, args);
+        SpringApplication.run(ProducerApplication.class, args);
     }
 
 }
