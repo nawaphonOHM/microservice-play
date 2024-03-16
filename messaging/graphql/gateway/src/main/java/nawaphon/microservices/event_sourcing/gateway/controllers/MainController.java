608e9872f24422c11181cf88ee30168bad53541b
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-03-16 20:16:30 +0700

    Implement GraphQL API for querying customer data
    Extended the MainController to support GraphQL query operations. Implemented two query endpoints, 'getCustomer' and 'getCustomerDetails', that retrieve data from the 'Customer' and 'CustomerDetail' classes, using the FakeDatabaseComponent. Now, the customers' data can be fetched by ID, improving the flexibility and efficiency of data retrieval.

--- messaging/graphql/gateway/src/main/java/nawaphon/microservices/event_sourcing/gateway/controllers/MainController.java
+++ messaging/graphql/gateway/src/main/java/nawaphon/microservices/event_sourcing/gateway/controllers/MainController.java
@@ -1,9 +1,16 @@
 package nawaphon.microservices.event_sourcing.gateway.controllers;
 
 import nawaphon.microservices.event_sourcing.gateway.components.FakeDatabaseComponent;
+import nawaphon.microservices.event_sourcing.gateway.pojo.Customer;
+import nawaphon.microservices.event_sourcing.gateway.pojo.CustomerDetail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class MainController {
@@ -14,5 +21,18 @@ public class MainController {
 
     public MainController(final FakeDatabaseComponent fakeDatabaseComponent) {
         this.fakeDatabaseComponent = fakeDatabaseComponent;
+    }
+
+    @QueryMapping()
+    public Customer getCustomer(@Argument final UUID id) {
+        return fakeDatabaseComponent.getCustomers().stream().filter((var1) -> var1.getId().compareTo(id) == 0)
+                .findFirst().orElse(new Customer());
+    }
+
+    @SchemaMapping()
+    public CustomerDetail getCustomerDetails(final Customer customer) {
+        return fakeDatabaseComponent.getCustomerDetails().stream()
+                .filter((var1) -> var1.getCustomerId().compareTo(customer.getDetailsId()) == 0)
+                .findFirst().orElse(new CustomerDetail());
     }
 }
