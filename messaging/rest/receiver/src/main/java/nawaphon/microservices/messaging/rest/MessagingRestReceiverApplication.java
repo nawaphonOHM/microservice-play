3147751f78458932eabcb50ca7b3368a7882fd12
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-04-10 12:00:59 +0700

    Rename ReceiverApplication to MessagingRestReceiverApplication
    In order to more accurately reflect its role in the system, the ReceiverApplication class has been renamed to MessagingRestReceiverApplication. This change improves consistency and clarity within the codebase.

--- messaging/rest/receiver/src/main/java/nawaphon/microservices/messaging/rest/MessagingRestReceiverApplication.java
+++ messaging/rest/receiver/src/main/java/nawaphon/microservices/messaging/rest/MessagingRestReceiverApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReceiverApplication {
+public class MessagingRestReceiverApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReceiverApplication.class, args);
+        SpringApplication.run(MessagingRestReceiverApplication.class, args);
     }
 
 }
