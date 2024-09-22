36fec07888585e823d2f3c361e6eb8070be258be
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-09-22 22:17:28 +0700

    Add MainController with /hello-world endpoint
    This commit introduces a new MainController class that provides a /hello-world endpoint. When accessed, this endpoint returns a ResponseMessage containing a greeting along with the hostname of the server processing the request. Error handling is included to manage potential UnknownHostException scenarios.

---
+++ client_side_discovery/serviceA/src/main/java/nawaphon/microservices/self_registration/service_a/controllers/MainController.java
@@ -0,0 +1,35 @@
+package nawaphon.microservices.self_registration.service_a.controllers;
+
+import nawaphon.microservices.self_registration.service_a.pojo.ResponseMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+@RestController
+public class MainController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+
+    @GetMapping("/hello-world")
+    public ResponseMessage<String> home() {
+        final String hostname;
+
+        try {
+            hostname = InetAddress.getLocalHost().getHostName();
+        } catch (final UnknownHostException e) {
+            logger.error(e.getMessage(), e);
+            return new ResponseMessage<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Unable to complete request");
+        }
+        return new ResponseMessage<>(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+                String.format("Hello world this request was processed by %s", hostname));
+    }
+}
