53da85d00e7827779726a3987fc1c2574230072a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-09-24 22:20:33 +0700

    Fix typo in service registry jar file path
    Corrected the file path from "services_registry" to "service_registry" in entrypoint.sh. This ensures the Java service starts correctly without path errors.

--- client_side_discovery/services_registry/entrypoint.sh
+++ client_side_discovery/services_registry/entrypoint.sh
@@ -12,7 +12,7 @@ main() {
 
   unset ERROR
 
-  java -jar /home/services_registry/services_registry-0.0.1.jar || exit 1
+  java -jar /home/service_registry/service_registry-0.0.1.jar || exit 1
 }
 
 mandatoryEnvCheck() {
