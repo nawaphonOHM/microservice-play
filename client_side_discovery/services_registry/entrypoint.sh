09833f46fa6bccc08900c78d5299fe3aeb403625
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-09-24 22:08:45 +0700

    Create service registry with Spring Boot
    Added a new Spring Boot application for the service registry under client_side_discovery. This includes the application.properties configuration, a basic build.gradle file, and the main application class.

---
+++ client_side_discovery/services_registry/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+main() {
+
+  export ERROR=0
+
+  mandatoryEnvCheck
+
+  if [ "$ERROR" -eq 1 ]; then
+        exit 1
+  fi
+
+  unset ERROR
+
+  java -jar /home/services_registry/services_registry-0.0.1.jar || exit 1
+}
+
+mandatoryEnvCheck() {
+  ERROR=0
+}
+
+
+
+main
---
+++ client_side_discovery/services_registry/src/main/java/nawaphon/microservices/client_side_discovery/service_registry/ServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package nawaphon.microservices.client_side_discovery.service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
