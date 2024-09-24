083ab300b2af051320adf6fe1788df94a98a21c5
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-09-24 22:12:31 +0700

    Enable Eureka Server in ServiceRegistryApplication
    Added the `@EnableEurekaServer` annotation to the main application class to establish the service as a Eureka server. This change is necessary for service discovery and registration in a microservices architecture.

--- client_side_discovery/services_registry/src/main/java/nawaphon/microservices/client_side_discovery/service_registry/ServiceRegistryApplication.java
+++ client_side_discovery/services_registry/src/main/java/nawaphon/microservices/client_side_discovery/service_registry/ServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package nawaphon.microservices.client_side_discovery.service_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
