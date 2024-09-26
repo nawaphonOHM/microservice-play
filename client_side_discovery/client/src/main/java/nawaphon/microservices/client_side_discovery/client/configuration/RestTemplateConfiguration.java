57b0c2d7dcd04314ed8dc286f28474dd0a248294
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-09-26 23:59:22 +0700

    Add RestTemplate configuration with load balancing
    This commit introduces a new `RestTemplateConfiguration` class to the project. It provides a `RestTemplate` bean annotated with `@LoadBalanced` to enable client-side load balancing. This enhancement will distribute requests across available instances when using the `RestTemplate`.

---
+++ client_side_discovery/client/src/main/java/nawaphon/microservices/client_side_discovery/client/configuration/RestTemplateConfiguration.java
@@ -0,0 +1,17 @@
+package nawaphon.microservices.client_side_discovery.client.configuration;
+
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
