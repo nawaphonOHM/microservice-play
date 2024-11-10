d1a83661414cd41f3b6840b0dae9279ca76710f4
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-11-10 22:06:28 +0700

    Format long command lines in AWS setup script
    Break long `aws ec2 create-subnet` commands into multiple lines for better readability and maintainability. This change ensures the script remains readable and easier to manage in the future.

--- server_side_discovery/aws_cloud/setup_script.sh
+++ server_side_discovery/aws_cloud/setup_script.sh
@@ -50,7 +50,8 @@ fi
 echo "Create VPC is done ID=$VPC_ID"
 
 echo "Creating Subnet...0/2"
-SUBNET_ID_1=$(aws ec2 create-subnet --vpc-id "$VPC_ID" --cidr-block 192.168.0.0/24 --availability-zone-id apse1-az1  --tag-specifications '[{"ResourceType":"subnet","Tags":[{"Key":"Name","Value":"Subnet1"}]}]' )
+SUBNET_ID_1=$(aws ec2 create-subnet --vpc-id "$VPC_ID" --cidr-block 192.168.0.0/24 --availability-zone-id apse1-az1  \
+--tag-specifications '[{"ResourceType":"subnet","Tags":[{"Key":"Name","Value":"Subnet1"}]}]' )
 
 if [[ $? -ne 0 ]]
 then
@@ -58,7 +59,8 @@ then
   exit 1
 fi
 
-SUBNET_ID_2=$(aws ec2 create-subnet --vpc-id "$VPC_ID" --cidr-block 192.168.1.0/24 --availability-zone-id apse1-az2 --tag-specifications '[{"ResourceType":"subnet","Tags":[{"Key":"Name","Value":"Subnet2"}]}]' )
+SUBNET_ID_2=$(aws ec2 create-subnet --vpc-id "$VPC_ID" --cidr-block 192.168.1.0/24 --availability-zone-id apse1-az2 \
+--tag-specifications '[{"ResourceType":"subnet","Tags":[{"Key":"Name","Value":"Subnet2"}]}]' )
 
 if [[ $? -ne 0 ]]
 then
