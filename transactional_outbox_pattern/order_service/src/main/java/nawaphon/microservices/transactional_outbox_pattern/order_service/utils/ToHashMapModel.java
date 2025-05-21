56096ee93181d1af7f1ae85292b7aa1ff3f0621f
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-05-22 01:08:12 +0700

    Add utility class for converting objects to HashMap
    Introduce `ToHashMapModel` utility class to convert objects into a `Map<String, Object>` representation using Java reflection. Includes error handling with logging and custom exception for conversion failures.

---
+++ transactional_outbox_pattern/order_service/src/main/java/nawaphon/microservices/transactional_outbox_pattern/order_service/utils/ToHashMapModel.java
@@ -0,0 +1,39 @@
+package nawaphon.microservices.transactional_outbox_pattern.order_service.utils;
+
+import nawaphon.microservices.transactional_outbox_pattern.order_service.ToHashMpModelException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class ToHashMapModel {
+
+    private static final Logger log = LoggerFactory.getLogger(ToHashMapModel.class);
+
+    public static Map<String, Object> convert(final Object object) {
+        final Map<String, Object> map = new HashMap<>();
+
+        for (final Field field : object.getClass().getDeclaredFields()) {
+            final String key = field.getName();
+
+            try {
+                final Object value = object.getClass()
+                        .getDeclaredMethod("get" + key.substring(0, 1).toUpperCase() + key.substring(1))
+                        .invoke(object);
+
+                map.put(key, value);
+
+
+            } catch (final IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+                log.error("Unable to convert object to map", e);
+                throw new ToHashMpModelException(e);
+            }
+
+        }
+
+        return map;
+    }
+}
