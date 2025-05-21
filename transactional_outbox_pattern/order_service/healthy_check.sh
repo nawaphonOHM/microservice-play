22eea4a653399cb2251967fad12b9c387ef9d256
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-05-22 02:32:25 +0700

    Fix health check script path and variable typo
    Updated the health check endpoint to match the correct path and fixed a typo in the RESULT variable. These changes ensure accurate health status detection for the order service.

--- transactional_outbox_pattern/order_service/healthy_check.sh
+++ transactional_outbox_pattern/order_service/healthy_check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-REULST=$(curl 'http://localhost:8080/actuator/health' -i -X GET -H 'Accept: application/json' | jq '.status')
+RESULT=$(curl 'http://localhost:8080/transactional_outbox_pattern/order-service/actuator/health' -i -X GET -H 'Accept: application/json' | grep -Eo '[A-Z]+')
 
-if [ "$REULST" != "UP" ]; then
+if [ "$RESULT" != "UP" ]; then
   exit 1
 fi
 
