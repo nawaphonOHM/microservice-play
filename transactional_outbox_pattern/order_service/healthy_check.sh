1cd8d183ff01c4596855a9a5f5d894a8c2e6a635
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-05-22 03:07:18 +0700

    Simplify health check status extraction in script.
    Updated the health check script to parse the JSON response directly for the "status" field instead of relying on a generic grep pattern. This improves accuracy and ensures the script aligns with the response format.

--- transactional_outbox_pattern/order_service/healthy_check.sh
+++ transactional_outbox_pattern/order_service/healthy_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-RESULT=$(curl 'http://localhost:8080/transactional_outbox_pattern/order-service/actuator/health' -i -X GET -H 'Accept: application/json' | grep -Eo '[A-Z]+')
+RESULT=$(curl 'http://localhost:8080/transactional_outbox_pattern/order-service/actuator/health' -s -i -X GET -H 'Accept: application/json' | grep -Eo '\{\"status\":\"([A-Z]+)\"\}' | sed -E 's/\{\"status\":\"([A-Z]+)\"\}/\1/')
 
 if [ "$RESULT" != "UP" ]; then
   exit 1
