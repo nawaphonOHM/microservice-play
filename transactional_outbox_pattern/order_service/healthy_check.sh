c30fca3c6bdd3cd0681542584f830f1ceb5cf07d
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-05-22 22:09:42 +0700

    "Fix line length in health check script for readability
    Split a long curl command into multiple lines to enhance readability and maintainability. This change has no functional impact on the script's behavior."

--- transactional_outbox_pattern/order_service/healthy_check.sh
+++ transactional_outbox_pattern/order_service/healthy_check.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-RESULT=$(curl 'http://localhost:8080/transactional_outbox_pattern/order-service/actuator/health' -s -i -X GET -H 'Accept: application/json' | grep -Eo '\{\"status\":\"([A-Z]+)\"\}' | sed -E 's/\{\"status\":\"([A-Z]+)\"\}/\1/')
+RESULT=$(curl 'http://localhost:8080/transactional_outbox_pattern/order-service/actuator/health' -s -i -X GET -H \
+'Accept: application/json' | grep -Eo '\{\"status\":\"([A-Z]+)\"\}' | sed -E 's/\{\"status\":\"([A-Z]+)\"\}/\1/')
 
 if [ "$RESULT" != "UP" ]; then
   exit 1
