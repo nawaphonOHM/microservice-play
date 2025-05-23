2033793c9b659ebbef2affed40ef30bea1579c2a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-05-23 23:34:33 +0700

    Add `price` column and safeguard `orders` table creation
    The `orders` table now includes a `price` column with a default value, ensuring proper handling of order pricing. Additionally, the `create table` statement is updated to use `if not exists`, preventing errors during repeated

--- transactional_outbox_pattern/necessary_query_for_order_database.sql
+++ transactional_outbox_pattern/necessary_query_for_order_database.sql
@@ -1,13 +1,14 @@
-create table orders
+create table if not exists orders
 (
-    id           uuid         not null
+    id           uuid                 not null
         constraint orders_pk
             primary key,
-    order_id     uuid         not null
+    order_id     uuid                 not null
         constraint orders_pk_2
             unique,
-    order_status varchar(255) not null,
-    order_name   varchar(255) not null
+    order_status varchar(255)         not null,
+    order_name   varchar(255)         not null,
+    price        numeric default 0.00 not null
 );
 
 create table if not exists order_outbox
