5f5a685e0d7b9dcc8967e132f44493e546c7d07b
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-05-24 23:06:33 +0700

    Refactor controller methods to return specific types.
    Updated methods in MainController to return more precise types (e.g., `Customer`, `List<Customer>`, or `UUID`) instead of `ResponseMessage<?>`. This improves type clarity and aligns response handling with method semantics.

--- shared_database/customer_service/src/main/java/nawaphon/microservices/customer_service/controllers/MainController.java
+++ shared_database/customer_service/src/main/java/nawaphon/microservices/customer_service/controllers/MainController.java
@@ -2,11 +2,11 @@ package nawaphon.microservices.customer_service.controllers;
 
 import nawaphon.microservices.customer_service.pojo.Customer;
 import nawaphon.microservices.customer_service.pojo.CustomerId;
-import nawaphon.microservices.customer_service.pojo.ResponseMessage;
 import nawaphon.microservices.customer_service.services.MainService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -22,22 +22,22 @@ public class MainController {
     }
 
     @PostMapping("/post-customer")
-    public ResponseMessage<?> postNewCustomer(@RequestBody final Customer newCustomer) {
+    public Customer postNewCustomer(@RequestBody final Customer newCustomer) {
         return mainService.addNewCustomer(newCustomer);
     }
 
     @GetMapping("/get-customer-by-criteria")
-    public ResponseMessage<?> getCustomerByCriteria(@RequestParam final Map<String, String> params) {
+    public List<Customer> getCustomerByCriteria(@RequestParam final Map<String, String> params) {
         return mainService.getCustomerByCriteria(params);
     }
 
     @PatchMapping("/update-customer-credit/{customer-uuid}")
-    public ResponseMessage<?> patchCustomerCredit(@PathVariable("customer-uuid") final UUID customerUUID, @RequestBody final CustomerId credit) {
+    public Customer patchCustomerCredit(@PathVariable("customer-uuid") final UUID customerUUID, @RequestBody final CustomerId credit) {
         return mainService.updateUserCredit(customerUUID, credit.credit());
     }
 
     @DeleteMapping("delete-customer-credit/{customer-uuid}")
-    public ResponseMessage<?> removeCustomer(@PathVariable("customer-uuid") final UUID customerUUID) {
+    public UUID removeCustomer(@PathVariable("customer-uuid") final UUID customerUUID) {
         return mainService.removeCustomer(customerUUID);
     }
 }
