c87f49ea2982d4bdf07c33d9d4dd1609ac8b5e7b
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-05-24 13:30:35 +0700

    Add exception handler for SavingOrderUnsuccessfulException
    Introduce a new handler for SavingOrderUnsuccessfulException in NonBusinessException. This ensures a proper HTTP 500 response with relevant error details is returned when this exception occurs.

--- transactional_outbox_pattern/transaction_log_tailing/order_service/src/main/java/nawaphon/microservices/transactional_outbox_pattern/order_service/exception_advice/NonBusinessException.java
+++ transactional_outbox_pattern/transaction_log_tailing/order_service/src/main/java/nawaphon/microservices/transactional_outbox_pattern/order_service/exception_advice/NonBusinessException.java
@@ -1,9 +1,11 @@
 package nawaphon.microservices.transactional_outbox_pattern.order_service.exception_advice;
 
+import nawaphon.microservices.transactional_outbox_pattern.order_service.exception.SavingOrderUnsuccessfulException;
 import nawaphon.microservices.transactional_outbox_pattern.order_service.exception.ToHashMpModelException;
 import nawaphon.microservices.transactional_outbox_pattern.order_service.dto.ResponseMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -17,5 +19,11 @@ public class NonBusinessException {
                 "There is something occur unexpectedly. Consult Software Developer to fix this",
                 null
         );
+    }
+
+    @ExceptionHandler(SavingOrderUnsuccessfulException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseMessage<String> buildMessage(final SavingOrderUnsuccessfulException exception) {
+        return new ResponseMessage<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), exception.getMessage());
     }
 }
