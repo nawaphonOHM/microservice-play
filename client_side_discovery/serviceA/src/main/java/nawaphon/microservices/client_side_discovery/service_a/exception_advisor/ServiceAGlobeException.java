44d3a829e282f502f64f63f342fe0ef571477f3a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-05-24 22:14:35 +0700

    Add global exception handler for ServiceA.
    Introduced a `RestControllerAdvice` to handle `UnknownHostRuntimeException` in ServiceA. This ensures consistent error responses and improves exception management for the service.

---
+++ client_side_discovery/serviceA/src/main/java/nawaphon/microservices/client_side_discovery/service_a/exception_advisor/ServiceAGlobeException.java
@@ -0,0 +1,16 @@
+package nawaphon.microservices.client_side_discovery.service_a.exception_advisor;
+
+import nawaphon.microservices.client_side_discovery.service_a.exception.UnknownHostRuntimeException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ServiceAGlobeException {
+
+
+
+    @ExceptionHandler(UnknownHostRuntimeException.class)
+    public String handleUnknownHostRuntimeException(final UnknownHostRuntimeException ex) {
+        return ex.getMessage();
+    }
+}
