16493af86cea17f4be579d310af217ed4057ea7d
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 15:44:49 +0700

    Add `getCustomer` method to RealServiceExchange interface

--- circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/http_exchanges/RealServiceExchange.java
+++ circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/http_exchanges/RealServiceExchange.java
@@ -1,8 +1,11 @@
 package nawaphon.microservices.circuit_breaker.proxy.http_exchanges;
 
 
+import nawaphon.microservices.circuit_breaker.proxy.pojo.Message;
 import org.springframework.web.service.annotation.HttpExchange;
 
 @HttpExchange("${service-ip}")
 public interface RealServiceExchange {
+
+    Message getCustomer();
 }
