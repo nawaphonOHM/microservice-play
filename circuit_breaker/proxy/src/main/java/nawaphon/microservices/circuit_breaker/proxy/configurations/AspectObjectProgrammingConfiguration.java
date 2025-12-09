1a99f33ffc27e28757812888669362f8e09800c0
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 15:56:46 +0700

    Add AspectObjectProgrammingConfiguration class stub in circuit_breaker module

---
+++ circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/configurations/AspectObjectProgrammingConfiguration.java
@@ -0,0 +1,9 @@
+package nawaphon.microservices.circuit_breaker.proxy.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+class AspectObjectProgrammingConfiguration {
+
+}
