e6b7001d799c32091e202dccb2157aa1c31c82cc
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 15:48:41 +0700

    Annotate RealServiceExchangeConfiguration with @ImportHttpServices to register RealServiceExchange interface

--- circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/configurations/RealServiceExchangeConfiguration.java
+++ circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/configurations/RealServiceExchangeConfiguration.java
@@ -1,9 +1,12 @@
 package nawaphon.microservices.circuit_breaker.proxy.configurations;
 
+import nawaphon.microservices.circuit_breaker.proxy.http_exchanges.RealServiceExchange;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.service.registry.ImportHttpServices;
 
 
 @Configuration
+@ImportHttpServices(RealServiceExchange.class)
 class RealServiceExchangeConfiguration {
 
 }
