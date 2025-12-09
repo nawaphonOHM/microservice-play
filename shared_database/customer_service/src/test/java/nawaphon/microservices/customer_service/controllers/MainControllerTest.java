e9d738cac4ff4386e33e2fdb477d62853fff5e86
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 22:17:31 +0700

    Replace `@MockBean` with `@MockitoBean` in `MainControllerTest`

--- shared_database/customer_service/src/test/java/nawaphon/microservices/customer_service/controllers/MainControllerTest.java
+++ shared_database/customer_service/src/test/java/nawaphon/microservices/customer_service/controllers/MainControllerTest.java
@@ -9,6 +9,7 @@ import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -23,7 +24,7 @@ public class MainControllerTest {
 
     private final MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     private final MainService service;
 
     @Autowired
