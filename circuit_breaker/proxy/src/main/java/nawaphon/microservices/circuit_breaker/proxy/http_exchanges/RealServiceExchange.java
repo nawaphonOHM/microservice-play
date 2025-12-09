609a0437e783ad385156ef2e711c2c3c0e453aec
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 15:43:50 +0700

    Annotate RealServiceExchange with @HttpExchange for dynamic service IP mapping

--- circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/http_exchanges/RealServiceExchange.java
+++ circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/http_exchanges/RealServiceExchange.java
@@ -1,5 +1,8 @@
 package nawaphon.microservices.circuit_breaker.proxy.http_exchanges;
 
 
+import org.springframework.web.service.annotation.HttpExchange;
+
+@HttpExchange("${service-ip}")
 public interface RealServiceExchange {
 }
