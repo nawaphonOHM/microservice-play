630da8f0d577e68530f70cef75c7bb0ffb75422d
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 21:49:22 +0700

    Add `getCustomer` method to `ReceiverExchange` interface

--- messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/http_exchanges/ReceiverExchange.java
+++ messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/http_exchanges/ReceiverExchange.java
@@ -1,8 +1,11 @@
 package nawaphon.microservices.messaging.rest.http_exchanges;
 
 
+import nawaphon.microservices.messaging.rest.pojo.Customer;
 import org.springframework.web.service.annotation.HttpExchange;
 
 @HttpExchange("${connection.friend-service}")
 public interface ReceiverExchange {
+
+    Customer getCustomer();
 }
