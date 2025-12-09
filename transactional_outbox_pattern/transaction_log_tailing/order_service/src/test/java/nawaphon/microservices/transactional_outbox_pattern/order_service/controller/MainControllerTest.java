9dc7dcfbe1766ea583bb129806c0ca15a1500ea7
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 22:22:28 +0700

    Fix `WebMvcTest` import path after package structure update in `MainControllerTest`

--- transactional_outbox_pattern/transaction_log_tailing/order_service/src/test/java/nawaphon/microservices/transactional_outbox_pattern/order_service/controller/MainControllerTest.java
+++ transactional_outbox_pattern/transaction_log_tailing/order_service/src/test/java/nawaphon/microservices/transactional_outbox_pattern/order_service/controller/MainControllerTest.java
@@ -7,8 +7,8 @@ import nawaphon.microservices.transactional_outbox_pattern.order_service.service
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
