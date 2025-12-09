f89881f6ae4916cf8d4a39d86cf9f77b4cc6ea51
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 16:37:17 +0700

    Add `@Bean` annotation to `httpChangeAspect` method in AspectObjectProgrammingConfiguration

--- circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/configurations/AspectObjectProgrammingConfiguration.java
+++ circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/configurations/AspectObjectProgrammingConfiguration.java
@@ -1,12 +1,15 @@
 package nawaphon.microservices.circuit_breaker.proxy.configurations;
 
 import nawaphon.microservices.circuit_breaker.proxy.aspect.HttpChangeAspect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 class AspectObjectProgrammingConfiguration {
 
+
+    @Bean
     public HttpChangeAspect httpChangeAspect() {
         return new HttpChangeAspect();
     }
