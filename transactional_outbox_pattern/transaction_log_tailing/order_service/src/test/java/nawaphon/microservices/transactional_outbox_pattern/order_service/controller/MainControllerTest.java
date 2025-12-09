27ac0c60d1f26e9a3d29e9927e751a008cb98ab0
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 22:22:40 +0700

    Replace `@MockBean` with `@MockitoBean` in `MainControllerTest` to fix bean mocking annotation

--- transactional_outbox_pattern/transaction_log_tailing/order_service/src/test/java/nawaphon/microservices/transactional_outbox_pattern/order_service/controller/MainControllerTest.java
+++ transactional_outbox_pattern/transaction_log_tailing/order_service/src/test/java/nawaphon/microservices/transactional_outbox_pattern/order_service/controller/MainControllerTest.java
@@ -7,9 +7,9 @@ import nawaphon.microservices.transactional_outbox_pattern.order_service.service
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -30,7 +30,7 @@ class MainControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private MainService mainService;
 
     @Test
