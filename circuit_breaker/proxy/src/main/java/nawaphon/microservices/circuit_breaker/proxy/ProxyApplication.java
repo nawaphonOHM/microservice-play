1cc25b7308ccc720eecb9ae6d943643cbd3e4b7d
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 15:52:52 +0700

    Annotate ProxyApplication with @EnableAspectJAutoProxy to enable proxy-based AOP support

--- circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/ProxyApplication.java
+++ circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/ProxyApplication.java
@@ -2,8 +2,10 @@ package nawaphon.microservices.circuit_breaker.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class ProxyApplication {
 
     public static void main(String[] args) {
