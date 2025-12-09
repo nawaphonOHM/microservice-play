245ee3df6668b5c2eb9c98f813014e66792e3979
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 21:51:01 +0700

    Update `getCustomer` method in `ReceiverExchange` to accept `uuid` as a path variable

--- messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/http_exchanges/ReceiverExchange.java
+++ messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/http_exchanges/ReceiverExchange.java
@@ -2,10 +2,15 @@ package nawaphon.microservices.messaging.rest.http_exchanges;
 
 
 import nawaphon.microservices.messaging.rest.pojo.Customer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
+
+import java.util.UUID;
 
 @HttpExchange("${connection.friend-service}")
 public interface ReceiverExchange {
 
-    Customer getCustomer();
+    @GetExchange("/get-customer/{uuid}")
+    Customer getCustomer(@PathVariable UUID uuid);
 }
