0b204aaa6f5e937d50ea1b7a94ca9ca6ffe830d5
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-09 21:17:21 +0700

    Annotate `logHttpChange` method with `@Before` to log HTTP exchanges

--- circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/aspect/HttpChangeAspect.java
+++ circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/aspect/HttpChangeAspect.java
@@ -2,10 +2,12 @@ package nawaphon.microservices.circuit_breaker.proxy.aspect;
 
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class HttpChangeAspect {
 
+    @Before("execution(* nawaphon.microservices.circuit_breaker.proxy.http_exchanges.*.*(..))")
     public void logHttpChange() {
 
     }
