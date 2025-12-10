2542e4b192a4ee201cd9e944af25fe940be96a82
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-11 00:30:04 +0700

    Add basic endpoints to `MainController` for retrieving customer and customer details

--- messaging/grpc/client/src/main/java/nawaphon/microservices/messaging/grpc/client/controllers/MainController.java
+++ messaging/grpc/client/src/main/java/nawaphon/microservices/messaging/grpc/client/controllers/MainController.java
@@ -1,8 +1,25 @@
 package nawaphon.microservices.messaging.grpc.client.controllers;
 
 
+import nawaphon.microservices.messaging.grpc.client.pojo.Customer;
+import nawaphon.microservices.messaging.grpc.client.pojo.CustomerDetail;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class MainController {
+
+    @GetMapping("/get-customer/{uuid}")
+    public Customer getCustomer(@PathVariable final UUID uuid) {
+
+        return null;
+    }
+
+    @GetMapping("/get-customer-details/{uuid}")
+    public CustomerDetail getCustomerDetail(@PathVariable final UUID uuid) {
+        return null;
+    }
 }
