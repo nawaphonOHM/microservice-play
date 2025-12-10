9d3afaad254c4c84b9af6a8b28ab7137171f335b
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 22:09:08 +0700

    Enable Spring AOP proxying with `@EnableAspectJAutoProxy` in `SpringAopConfiguration`

--- messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/SpringAopConfiguration.java
+++ messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/SpringAopConfiguration.java
@@ -3,9 +3,11 @@ package nawaphon.microservices.messaging.rest.configurations;
 import nawaphon.microservices.messaging.rest.aspect.HttpChangeAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @Configuration
+@EnableAspectJAutoProxy
 class SpringAopConfiguration {
 
 
