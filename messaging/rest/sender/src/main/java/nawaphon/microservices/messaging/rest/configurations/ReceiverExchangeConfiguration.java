e637964fa0a5be628f039a3b5752e5d5992d4c65
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 19:02:31 +0700

    Annotate `receiverExchange` method with `@Bean` in `ReceiverExchangeConfiguration`

--- messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/ReceiverExchangeConfiguration.java
+++ messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/ReceiverExchangeConfiguration.java
@@ -1,6 +1,7 @@
 package nawaphon.microservices.messaging.rest.configurations;
 
 import nawaphon.microservices.messaging.rest.http_exchanges.ReceiverExchange;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestClient;
 @Configuration
 class ReceiverExchangeConfiguration {
 
+    @Bean
     public ReceiverExchange receiverExchange(RestClient.Builder builder, ConnectionConfiguration connectionConfiguration) {
         return null;
     }
