551073723624855560e3e388ceb8a5390fcd1f60
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 19:18:19 +0700

    Define `realServiceExchange` bean with `RestClient.Builder` and service IP injection in `RealServiceExchangeConfiguration`

--- circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/configurations/RealServiceExchangeConfiguration.java
+++ circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/configurations/RealServiceExchangeConfiguration.java
@@ -1,9 +1,16 @@
 package nawaphon.microservices.circuit_breaker.proxy.configurations;
 
+import nawaphon.microservices.circuit_breaker.proxy.http_exchanges.RealServiceExchange;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 
 @Configuration
 class RealServiceExchangeConfiguration {
 
+
+    public RealServiceExchange realServiceExchange(RestClient.Builder builder, @Value("${service-ip}") String friendIp) {
+        return null;
+    }
 }
