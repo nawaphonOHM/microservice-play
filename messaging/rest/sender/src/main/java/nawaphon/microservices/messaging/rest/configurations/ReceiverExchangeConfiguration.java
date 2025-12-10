2a97dadb2287560bd77373e7a68547af58dc5223
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 19:07:49 +0700

    Enhance `receiverExchange` bean implementation in `ReceiverExchangeConfiguration` with `HttpServiceProxyFactory`

--- messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/ReceiverExchangeConfiguration.java
+++ messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/ReceiverExchangeConfiguration.java
@@ -4,6 +4,10 @@ import nawaphon.microservices.messaging.rest.http_exchanges.ReceiverExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.net.URI;
 
 
 @Configuration
@@ -11,7 +15,15 @@ class ReceiverExchangeConfiguration {
 
     @Bean
     public ReceiverExchange receiverExchange(RestClient.Builder builder, ConnectionConfiguration connectionConfiguration) {
-        return null;
+        final var restClient = builder.baseUrl(URI.create(connectionConfiguration.getFriendService())).build();
+
+        return HttpServiceProxyFactory
+                .builderFor(
+                        RestClientAdapter
+                                .create(restClient)
+                )
+                .build()
+                .createClient(ReceiverExchange.class);
     }
 
 }
