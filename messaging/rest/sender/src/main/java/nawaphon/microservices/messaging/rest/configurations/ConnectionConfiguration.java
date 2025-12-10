0216447e65b81dbd0f6b0b02be2b98ec187549fb
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 19:31:14 +0700

    Remove `@ConfigurationPropertiesScan` from `ConnectionConfiguration` to simplify configuration annotations

--- messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/ConnectionConfiguration.java
+++ messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/ConnectionConfiguration.java
@@ -1,11 +1,9 @@
 package nawaphon.microservices.messaging.rest.configurations;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 
 @ConfigurationProperties(prefix = "connection")
-@ConfigurationPropertiesScan
 public class ConnectionConfiguration {
     private String friendService;
 
