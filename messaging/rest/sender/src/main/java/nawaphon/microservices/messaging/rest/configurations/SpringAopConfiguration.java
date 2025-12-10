5a326d283afd8bf97bf7d5ddf73530369ab9339f
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 22:06:43 +0700

    Add `SpringAopConfiguration` with `@Configuration` annotation

---
+++ messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/SpringAopConfiguration.java
@@ -0,0 +1,9 @@
+package nawaphon.microservices.messaging.rest.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+class SpringAopConfiguration {
+
+}
