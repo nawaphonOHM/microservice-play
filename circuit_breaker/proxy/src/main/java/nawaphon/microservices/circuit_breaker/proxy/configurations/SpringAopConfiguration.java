804eef8fcdc7a401285ce9b800dea8e771279de0
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 22:43:16 +0700

    Define `httpChangeAspect` method in `SpringAopConfiguration`

--- circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/configurations/SpringAopConfiguration.java
+++ circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/configurations/SpringAopConfiguration.java
@@ -1,5 +1,6 @@
 package nawaphon.microservices.circuit_breaker.proxy.configurations;
 
+import nawaphon.microservices.circuit_breaker.proxy.aspect.HttpChangeAspect;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -7,5 +8,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Configuration
 @EnableAspectJAutoProxy
 class SpringAopConfiguration {
+
+
+    public HttpChangeAspect httpChangeAspect() {
+        return null;
+    }
 
 }
