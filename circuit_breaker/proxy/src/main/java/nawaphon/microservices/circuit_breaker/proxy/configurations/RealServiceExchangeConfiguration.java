dc096b00da92cb52563ed9de77610665f32fc621
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 19:15:04 +0700

    Remove unused `@ImportHttpServices` annotation in `RealServiceExchangeConfiguration`

--- circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/configurations/RealServiceExchangeConfiguration.java
+++ circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/configurations/RealServiceExchangeConfiguration.java
@@ -1,12 +1,9 @@
 package nawaphon.microservices.circuit_breaker.proxy.configurations;
 
-import nawaphon.microservices.circuit_breaker.proxy.http_exchanges.RealServiceExchange;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.service.registry.ImportHttpServices;
 
 
 @Configuration
-@ImportHttpServices(RealServiceExchange.class)
 class RealServiceExchangeConfiguration {
 
 }
