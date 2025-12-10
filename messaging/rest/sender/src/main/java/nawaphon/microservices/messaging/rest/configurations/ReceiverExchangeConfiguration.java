aef6ec2fff01f72b4fbb34b90823241b9926a290
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 17:57:52 +0700

    Add `receiverExchange` bean definition in `ReceiverExchangeConfiguration`

--- messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/ReceiverExchangeConfiguration.java
+++ messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/ReceiverExchangeConfiguration.java
@@ -1,9 +1,15 @@
 package nawaphon.microservices.messaging.rest.configurations;
 
+import nawaphon.microservices.messaging.rest.http_exchanges.ReceiverExchange;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 
 @Configuration
 class ReceiverExchangeConfiguration {
+
+    public ReceiverExchange receiverExchange(RestClient.Builder builder, ConnectionConfiguration connectionConfiguration) {
+        return null;
+    }
 
 }
