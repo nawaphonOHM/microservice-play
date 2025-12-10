ba0bd96be7b5720f40304242e0998171036196fd
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 19:21:24 +0700

    Remove unused `@HttpExchange` annotation in `RealServiceExchange` interface

--- circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/http_exchanges/RealServiceExchange.java
+++ circuit_breaker/proxy/src/main/java/nawaphon/microservices/circuit_breaker/proxy/http_exchanges/RealServiceExchange.java
@@ -3,9 +3,7 @@ package nawaphon.microservices.circuit_breaker.proxy.http_exchanges;
 
 import nawaphon.microservices.circuit_breaker.proxy.pojo.Message;
 import org.springframework.web.service.annotation.GetExchange;
-import org.springframework.web.service.annotation.HttpExchange;
 
-@HttpExchange("${service-ip}")
 public interface RealServiceExchange {
 
     @GetExchange("/first-get")
