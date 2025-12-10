79ce7540d0b1998021cdf9c8a7d4dee54a902127
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 17:57:25 +0700

    Remove unused `ImportHttpServices` annotation in `ReceiverExchangeConfiguration`

--- messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/ReceiverExchangeConfiguration.java
+++ messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/ReceiverExchangeConfiguration.java
@@ -1,12 +1,9 @@
 package nawaphon.microservices.messaging.rest.configurations;
 
-import nawaphon.microservices.messaging.rest.http_exchanges.ReceiverExchange;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.service.registry.ImportHttpServices;
 
 
 @Configuration
-@ImportHttpServices(ReceiverExchange.class)
 class ReceiverExchangeConfiguration {
 
 }
