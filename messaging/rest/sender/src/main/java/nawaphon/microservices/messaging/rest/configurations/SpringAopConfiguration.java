baeb4a6e9543c8626934a872cc4156dd4e914de6
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-10 22:07:26 +0700

    Add `httpChangeAspect` bean definition in `SpringAopConfiguration`

--- messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/SpringAopConfiguration.java
+++ messaging/rest/sender/src/main/java/nawaphon/microservices/messaging/rest/configurations/SpringAopConfiguration.java
@@ -1,9 +1,15 @@
 package nawaphon.microservices.messaging.rest.configurations;
 
+import nawaphon.microservices.messaging.rest.aspect.HttpChangeAspect;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 class SpringAopConfiguration {
+
+
+    public HttpChangeAspect httpChangeAspect() {
+        return null;
+    }
 
 }
