9d72392db0b0cb9831c9a794b81c0246eedb9f83
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-11 23:01:56 +0700

    Add `FakeDatabaseComponent` to generate and log mock customer data for testing purposes

---
+++ messaging/grpc/server/src/main/java/nawaphon/microservices/messaging/grpc/server/components/FakeDatabaseComponent.java
@@ -0,0 +1,75 @@
+package nawaphon.microservices.messaging.grpc.server.components;
+
+import jakarta.annotation.PostConstruct;
+import nawaphon.microservices.messaging.grpc.server.pojo.Customer;
+import nawaphon.microservices.messaging.grpc.server.pojo.CustomerDetail;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import tools.jackson.core.JacksonException;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class FakeDatabaseComponent {
+    private static final Logger logger = LogManager.getLogger(FakeDatabaseComponent.class);
+
+    private final List<Customer> customers = new ArrayList<>();
+    private final List<CustomerDetail> customerDetails = new ArrayList<>();
+
+    private final ObjectMapper objectMapper;
+
+    public FakeDatabaseComponent(final ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public List<CustomerDetail> getCustomerDetails() {
+        return customerDetails;
+    }
+
+    @PostConstruct
+    public void makeData() {
+        {
+            final var customerFake = new Customer(UUID.randomUUID(), UUID.randomUUID());
+            final var customerDetailFake = new CustomerDetail(customerFake.detailsId(), "John", "Doe");
+            customers.add(customerFake);
+            customerDetails.add(customerDetailFake);
+            try {
+                logger.info("Customer and CustomerDetail added: {}", objectMapper.writeValueAsString(customerFake));
+            } catch (JacksonException e) {
+                throw new RuntimeException("Unable to serialize value: ", e);
+            }
+        }
+
+        {
+            final var customerFake = new Customer(UUID.randomUUID(), UUID.randomUUID());
+            final var customerDetailFake = new CustomerDetail(customerFake.detailsId(), "Jane", "Smith");
+            customers.add(customerFake);
+            customerDetails.add(customerDetailFake);
+            try {
+                logger.info("Customer and CustomerDetail added: {}", objectMapper.writeValueAsString(customerFake));
+            } catch (JacksonException e) {
+                throw new RuntimeException("Unable to serialize value: ", e);
+            }
+        }
+
+        {
+            final var customerFake = new Customer(UUID.randomUUID(), UUID.randomUUID());
+            final var customerDetailFake = new CustomerDetail(customerFake.detailsId(), "Bob", "Johnson");
+            customers.add(customerFake);
+            customerDetails.add(customerDetailFake);
+            try {
+                logger.info("Customer and CustomerDetail added: {}", objectMapper.writeValueAsString(customerFake));
+            } catch (JacksonException e) {
+                throw new RuntimeException("Unable to serialize value:", e);
+            }
+        }
+    }
+}
