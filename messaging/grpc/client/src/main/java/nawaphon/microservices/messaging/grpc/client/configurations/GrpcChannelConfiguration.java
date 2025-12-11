808854601e951b3c45b16d80de9eae300bc3faa7
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-11 20:57:03 +0700

    Annotate `mainServerBlockingV2Stub` with `@Bean` in `GrpcChannelConfiguration` to enable Spring Bean management

--- messaging/grpc/client/src/main/java/nawaphon/microservices/messaging/grpc/client/configurations/GrpcChannelConfiguration.java
+++ messaging/grpc/client/src/main/java/nawaphon/microservices/messaging/grpc/client/configurations/GrpcChannelConfiguration.java
@@ -1,6 +1,7 @@
 package nawaphon.microservices.messaging.grpc.client.configurations;
 
 import nawaphon.microservices.messaging.grpc.client.MainServerGrpc;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.grpc.client.GrpcChannelFactory;
 
@@ -9,6 +10,7 @@ import org.springframework.grpc.client.GrpcChannelFactory;
 class GrpcChannelConfiguration {
 
 
+    @Bean
     public MainServerGrpc.MainServerBlockingV2Stub mainServerBlockingV2Stub(GrpcChannelFactory channels) {
         return null;
     }
