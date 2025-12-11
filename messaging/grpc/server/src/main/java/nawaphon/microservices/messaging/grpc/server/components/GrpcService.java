1f05ac9c69883664fc50cbc0220c9bc24bfb48cb
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-11 23:06:57 +0700

    Implement `customer` and `customerDetail` RPC methods in `GrpcService`

--- messaging/grpc/server/src/main/java/nawaphon/microservices/messaging/grpc/server/components/GrpcService.java
+++ messaging/grpc/server/src/main/java/nawaphon/microservices/messaging/grpc/server/components/GrpcService.java
@@ -1,10 +1,22 @@
 package nawaphon.microservices.messaging.grpc.server.components;
 
+import io.grpc.stub.StreamObserver;
+import nawaphon.microservices.messaging.grpc.server.CustomerDetailMessage;
+import nawaphon.microservices.messaging.grpc.server.CustomerMessage;
 import nawaphon.microservices.messaging.grpc.server.MainServerGrpc;
+import nawaphon.microservices.messaging.grpc.server.UUID;
 import org.springframework.stereotype.Component;
 
 
 @Component
 class GrpcService extends MainServerGrpc.MainServerImplBase {
+    @Override
+    public void customer(UUID request, StreamObserver<CustomerMessage> responseObserver) {
+        super.customer(request, responseObserver);
+    }
 
+    @Override
+    public void customerDetail(UUID request, StreamObserver<CustomerDetailMessage> responseObserver) {
+        super.customerDetail(request, responseObserver);
+    }
 }
