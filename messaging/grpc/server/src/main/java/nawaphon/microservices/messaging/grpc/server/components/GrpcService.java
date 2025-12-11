a6ec3eb6e14e2b1a5178eaf9464523ee2f8f0eed
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-11 23:16:22 +0700

    Add error handling and logging to `customer` RPC method in `GrpcService`

--- messaging/grpc/server/src/main/java/nawaphon/microservices/messaging/grpc/server/components/GrpcService.java
+++ messaging/grpc/server/src/main/java/nawaphon/microservices/messaging/grpc/server/components/GrpcService.java
@@ -5,12 +5,18 @@ import nawaphon.microservices.messaging.grpc.server.CustomerDetailMessage;
 import nawaphon.microservices.messaging.grpc.server.CustomerMessage;
 import nawaphon.microservices.messaging.grpc.server.MainServerGrpc;
 import nawaphon.microservices.messaging.grpc.server.UUID;
+import org.jspecify.annotations.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.NoSuchElementException;
 
 
 @Component
 class GrpcService extends MainServerGrpc.MainServerImplBase {
 
+    private static final Logger log = LoggerFactory.getLogger(GrpcService.class);
     private final FakeDatabaseComponent fakeDatabaseComponent;
 
     GrpcService(FakeDatabaseComponent fakeDatabaseComponent) {
@@ -18,8 +24,28 @@ class GrpcService extends MainServerGrpc.MainServerImplBase {
     }
 
     @Override
-    public void customer(UUID request, StreamObserver<CustomerMessage> responseObserver) {
-        super.customer(request, responseObserver);
+    public void customer(UUID request, @NonNull StreamObserver<CustomerMessage> responseObserver) {
+
+        try {
+            final var customer = fakeDatabaseComponent.getCustomers()
+                    .stream().filter(it -> it.id().equals(java.util.UUID.fromString(request.getValue())))
+                    .findFirst().orElseThrow();
+
+            responseObserver.onNext(
+                    CustomerMessage.newBuilder()
+                            .setId(UUID.newBuilder().setValue(customer.id().toString()).build())
+                            .setDetailsId(UUID.newBuilder().setValue(customer.detailsId().toString()).build())
+                            .build()
+            );
+
+            responseObserver.onCompleted();
+
+
+        } catch (NoSuchElementException e) {
+            log.error("Cannot find customer with id: {}", request.getValue());
+            responseObserver.onError(e);
+        }
+
     }
 
     @Override
