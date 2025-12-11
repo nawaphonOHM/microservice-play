268b07f90265ac6f547f6a2e54a56dc83850bb4c
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-11 21:13:04 +0700

    Annotate `handleStatusException` method in `GrpcRestControllerAdvice` with `@ExceptionHandler` for improved gRPC exception handling

--- messaging/grpc/client/src/main/java/nawaphon/microservices/messaging/grpc/client/rest_controller_advices/GrpcRestControllerAdvice.java
+++ messaging/grpc/client/src/main/java/nawaphon/microservices/messaging/grpc/client/rest_controller_advices/GrpcRestControllerAdvice.java
@@ -1,6 +1,7 @@
 package nawaphon.microservices.messaging.grpc.client.rest_controller_advices;
 
 import io.grpc.StatusException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 class GrpcRestControllerAdvice {
 
 
+    @ExceptionHandler(StatusException.class)
     public void handleStatusException(StatusException exception) {
 
     }
