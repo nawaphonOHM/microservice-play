e8fa41cc299bcd1826d5d2ba7f9cd5bf7234913f
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-11 21:14:40 +0700

    Add logging and @NonNull annotation to `handleStatusException` in `GrpcRestControllerAdvice` for improved error handling

--- messaging/grpc/client/src/main/java/nawaphon/microservices/messaging/grpc/client/rest_controller_advices/GrpcRestControllerAdvice.java
+++ messaging/grpc/client/src/main/java/nawaphon/microservices/messaging/grpc/client/rest_controller_advices/GrpcRestControllerAdvice.java
@@ -1,6 +1,9 @@
 package nawaphon.microservices.messaging.grpc.client.rest_controller_advices;
 
 import io.grpc.StatusException;
+import org.jspecify.annotations.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,10 +14,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 class GrpcRestControllerAdvice {
 
 
+    private static final Logger log = LoggerFactory.getLogger(GrpcRestControllerAdvice.class);
+
     @ExceptionHandler(StatusException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public void handleStatusException(StatusException exception) {
-
+    public void handleStatusException(@NonNull StatusException exception) {
+        log.error("gRPC error: {}", exception.getMessage());
     }
 
 }
