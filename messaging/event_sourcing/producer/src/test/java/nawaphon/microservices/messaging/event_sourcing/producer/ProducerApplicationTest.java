21d38fbe4e908cb258a119ff73b3c563a443b235
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-23 01:02:23 +0700

    Inject `ApplicationContext` into `ProducerApplicationTest` constructor for improved test setup.

--- messaging/event_sourcing/producer/src/test/java/nawaphon/microservices/messaging/event_sourcing/producer/ProducerApplicationTest.java
+++ messaging/event_sourcing/producer/src/test/java/nawaphon/microservices/messaging/event_sourcing/producer/ProducerApplicationTest.java
@@ -2,11 +2,18 @@ package nawaphon.microservices.messaging.event_sourcing.producer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class ProducerApplicationTest {
+
+    private final ApplicationContext context;
+
+    ProducerApplicationTest(ApplicationContext context) {
+        this.context = context;
+    }
 
     @Test
     void contextLoads() {
