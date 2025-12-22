3eb1e55e2c3b870da034d9e8d1ddff0c16eeb615
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-23 00:58:05 +0700

    Add empty test class `ConsumerApplicationTest` for consumer service

---
+++ messaging/event_sourcing/consumer/src/test/java/nawaphon/microservices/event_sourcing/consumer/ConsumerApplicationTest.java
@@ -0,0 +1,10 @@
+package nawaphon.microservices.event_sourcing.consumer;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(properties = {"spring.profiles.active=test"})
+class ConsumerApplicationTest {
+
+}
