05993df9c36877907493d8af703b2d8d94f28d7e
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-22 19:51:28 +0700

    Annotate `ClientApplicationTest` with `@SpringBootTest` and set active profile to `test`.

--- client_side_discovery/client/src/test/java/nawaphon/microservices/client_side_discovery/client/ClientApplicationTest.java
+++ client_side_discovery/client/src/test/java/nawaphon/microservices/client_side_discovery/client/ClientApplicationTest.java
@@ -1,8 +1,10 @@
 package nawaphon.microservices.client_side_discovery.client;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@SpringBootTest(properties = {"spring.profiles.active=test"})
 class ClientApplicationTest {
 
 }
