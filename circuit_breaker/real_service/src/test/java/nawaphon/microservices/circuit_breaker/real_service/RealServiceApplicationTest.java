949d243d6675c048fb5aa0bce16399eb16609acd
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-22 19:44:13 +0700

    Add empty `RealServiceApplicationTest` with `@SpringBootTest` annotation

---
+++ circuit_breaker/real_service/src/test/java/nawaphon/microservices/circuit_breaker/real_service/RealServiceApplicationTest.java
@@ -0,0 +1,10 @@
+package nawaphon.microservices.circuit_breaker.real_service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(properties = {"spring.profiles.active=test"})
+class RealServiceApplicationTest {
+
+}
