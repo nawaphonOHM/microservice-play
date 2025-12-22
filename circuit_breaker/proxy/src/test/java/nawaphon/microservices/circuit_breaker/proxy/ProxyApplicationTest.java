e092e082be8b41c82a9fa74223aab5f672ea5777
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-22 19:21:44 +0700

    Add initial test class for ProxyApplication

---
+++ circuit_breaker/proxy/src/test/java/nawaphon/microservices/circuit_breaker/proxy/ProxyApplicationTest.java
@@ -0,0 +1,11 @@
+package nawaphon.microservices.circuit_breaker.proxy;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class ProxyApplicationTest {
+
+}
