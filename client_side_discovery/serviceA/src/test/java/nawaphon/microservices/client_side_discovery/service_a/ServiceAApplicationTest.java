3c11ef16e430412117f3a92e0960f6d31f854722
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-22 20:37:39 +0700

    Add empty `contextLoads` test method in `ServiceAApplicationTest`

--- client_side_discovery/serviceA/src/test/java/nawaphon/microservices/client_side_discovery/service_a/ServiceAApplicationTest.java
+++ client_side_discovery/serviceA/src/test/java/nawaphon/microservices/client_side_discovery/service_a/ServiceAApplicationTest.java
@@ -7,4 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class ServiceAApplicationTest {
 
+    void contextLoads() {
+    }
+
 }
