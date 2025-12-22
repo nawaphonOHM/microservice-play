ae14abc001718ff88c32d6469c8e7482f7fab2e8
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-22 20:46:44 +0700

    Remove `@ConfigurationPropertiesScan` from `ConsumerApplication`

--- messaging/event_sourcing/consumer/src/main/java/nawaphon/microservices/event_sourcing/consumer/ConsumerApplication.java
+++ messaging/event_sourcing/consumer/src/main/java/nawaphon/microservices/event_sourcing/consumer/ConsumerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan(basePackages = "nawaphon.microservices.event_sourcing.consumer.configurations")
 public class ConsumerApplication {
 
     public static void main(String[] args) {
