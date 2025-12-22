50860f4df6506eeb02384becfe279b4902d057fe
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-22 20:36:44 +0700

    Add empty test class `ServiceAApplicationTest` for service A

---
+++ client_side_discovery/serviceA/src/test/java/nawaphon/microservices/client_side_discovery/service_a/ServiceAApplicationTest.java
@@ -0,0 +1,10 @@
+package nawaphon.microservices.client_side_discovery.service_a;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ServiceAApplicationTest {
+
+}
