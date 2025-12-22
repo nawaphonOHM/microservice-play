564d507abe51fa5c4afb527ef10b264576b9e891
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-23 02:12:01 +0700

    Annotate `GrpcClientApplicationTest` with `@SpringBootTest` and set `test` profile properties

--- messaging/grpc/client/src/test/java/nawaphon/microservices/messaging/grpc/client/GrpcClientApplicationTest.java
+++ messaging/grpc/client/src/test/java/nawaphon/microservices/messaging/grpc/client/GrpcClientApplicationTest.java
@@ -1,9 +1,12 @@
 package nawaphon.microservices.messaging.grpc.client;
 
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
+@SpringBootTest(properties = {"spring.profiles.active=test"})
 class GrpcClientApplicationTest {
 
 }
