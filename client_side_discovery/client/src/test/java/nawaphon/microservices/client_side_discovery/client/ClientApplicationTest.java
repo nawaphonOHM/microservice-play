59013643b5123639e6ff87d405c0a1c63a299725
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-22 19:52:13 +0700

    Inject `ApplicationContext` into `ClientApplicationTest` constructor for improved test setup.

--- client_side_discovery/client/src/test/java/nawaphon/microservices/client_side_discovery/client/ClientApplicationTest.java
+++ client_side_discovery/client/src/test/java/nawaphon/microservices/client_side_discovery/client/ClientApplicationTest.java
@@ -2,11 +2,18 @@ package nawaphon.microservices.client_side_discovery.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class ClientApplicationTest {
+
+    private final ApplicationContext context;
+
+    ClientApplicationTest(ApplicationContext context) {
+        this.context = context;
+    }
 
 
     @Test
