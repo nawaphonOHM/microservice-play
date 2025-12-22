132479056872b40bbd7174f9b8c2c78fb4f9ce54
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-23 00:58:24 +0700

    Inject `ApplicationContext` into `ConsumerApplicationTest` constructor for improved test setup.

--- messaging/event_sourcing/consumer/src/test/java/nawaphon/microservices/event_sourcing/consumer/ConsumerApplicationTest.java
+++ messaging/event_sourcing/consumer/src/test/java/nawaphon/microservices/event_sourcing/consumer/ConsumerApplicationTest.java
@@ -1,10 +1,16 @@
 package nawaphon.microservices.event_sourcing.consumer;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class ConsumerApplicationTest {
 
+    private final ApplicationContext context;
+
+    ConsumerApplicationTest(ApplicationContext context) {
+        this.context = context;
+    }
 }
