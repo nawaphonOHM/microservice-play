4759df25affd4b978aea2f11319f9a840632ebcc
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-22 20:41:09 +0700

    Add empty test class `ServiceRegistryApplicationTest` for services registry

---
+++ client_side_discovery/services_registry/src/test/java/nawaphon/microservices/client_side_discovery/service_registry/ServiceRegistryApplicationTest.java
@@ -0,0 +1,10 @@
+package nawaphon.microservices.client_side_discovery.service_registry;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(properties = {"spring.profiles.active=test"})
+class ServiceRegistryApplicationTest {
+
+}
