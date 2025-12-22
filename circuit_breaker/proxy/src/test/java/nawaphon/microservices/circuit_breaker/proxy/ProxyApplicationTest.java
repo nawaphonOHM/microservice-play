dd9cf0bacef862c7790427b339d1816aae52b54e
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-22 19:22:39 +0700

    Inject `ApplicationContext` into `ProxyApplicationTest` constructor

--- circuit_breaker/proxy/src/test/java/nawaphon/microservices/circuit_breaker/proxy/ProxyApplicationTest.java
+++ circuit_breaker/proxy/src/test/java/nawaphon/microservices/circuit_breaker/proxy/ProxyApplicationTest.java
@@ -1,6 +1,7 @@
 package nawaphon.microservices.circuit_breaker.proxy;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,4 +9,10 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ProxyApplicationTest {
 
+
+    private final ApplicationContext context;
+
+    ProxyApplicationTest(ApplicationContext context) {
+        this.context = context;
+    }
 }
