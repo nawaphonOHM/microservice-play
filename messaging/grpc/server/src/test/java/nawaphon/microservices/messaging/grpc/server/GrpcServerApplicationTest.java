39051a6162b93aa3a3e05847c4ad00552b4de019
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-25 20:42:48 +0700

    Inject `ApplicationContext` into `GrpcServerApplicationTest` and update constructor accordingly

--- messaging/grpc/server/src/test/java/nawaphon/microservices/messaging/grpc/server/GrpcServerApplicationTest.java
+++ messaging/grpc/server/src/test/java/nawaphon/microservices/messaging/grpc/server/GrpcServerApplicationTest.java
@@ -2,12 +2,19 @@ package nawaphon.microservices.messaging.grpc.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class GrpcServerApplicationTest {
+
+    private final ApplicationContext context;
+
+    GrpcServerApplicationTest(ApplicationContext context) {
+        this.context = context;
+    }
 
 
     @Test
