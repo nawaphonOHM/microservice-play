b4413f0175ce902caad778eb7d08454bc339e0c1
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-25 21:18:04 +0700

    Inject `ApplicationContext` into `CustomerServiceApplicationTest` via constructor

--- shared_database/customer_service/src/test/java/nawaphon/microservices/customer_service/CustomerServiceApplicationTest.java
+++ shared_database/customer_service/src/test/java/nawaphon/microservices/customer_service/CustomerServiceApplicationTest.java
@@ -1,10 +1,16 @@
 package nawaphon.microservices.customer_service;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class CustomerServiceApplicationTest {
 
+    private final ApplicationContext context;
+
+    CustomerServiceApplicationTest(ApplicationContext context) {
+        this.context = context;
+    }
 }
