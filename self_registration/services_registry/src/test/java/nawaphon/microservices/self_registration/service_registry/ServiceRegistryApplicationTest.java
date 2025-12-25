720185e996487c14a9e968aa576c4f202b9407d6
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-25 21:09:58 +0700

    Annotate `contextLoads` test in `ServiceRegistryApplicationTest` with `@Test`

--- self_registration/services_registry/src/test/java/nawaphon/microservices/self_registration/service_registry/ServiceRegistryApplicationTest.java
+++ self_registration/services_registry/src/test/java/nawaphon/microservices/self_registration/service_registry/ServiceRegistryApplicationTest.java
@@ -1,5 +1,6 @@
 package nawaphon.microservices.self_registration.service_registry;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class ServiceRegistryApplicationTest {
 
+    @Test
     void contextLoads() {
     }
 }
