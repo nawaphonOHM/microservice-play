580e02970db3d056a802d16830a48f6263ca518a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-25 21:19:49 +0700

    Annotate `OrderServiceApplicationTest` with `@SpringBootTest` and set `test` profile properties

--- shared_database/order_service/src/test/java/nawaphon/microservices/order_service/OrderServiceApplicationTest.java
+++ shared_database/order_service/src/test/java/nawaphon/microservices/order_service/OrderServiceApplicationTest.java
@@ -1,8 +1,10 @@
 package nawaphon.microservices.order_service;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@SpringBootTest(properties = {"spring.profiles.active=test"})
 class OrderServiceApplicationTest {
 
 }
