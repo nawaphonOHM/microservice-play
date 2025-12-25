4de103af2291597043805b37e1d9fd8f847fec75
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-25 20:59:29 +0700

    Annotate `contextLoads` test in `MessagingRestSenderApplicationTest` with `@Test`

--- messaging/rest/sender/src/test/java/nawaphon/microservices/messaging/rest/MessagingRestSenderApplicationTest.java
+++ messaging/rest/sender/src/test/java/nawaphon/microservices/messaging/rest/MessagingRestSenderApplicationTest.java
@@ -1,5 +1,6 @@
 package nawaphon.microservices.messaging.rest;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class MessagingRestSenderApplicationTest {
 
+    @Test
     void contextLoads() {}
 
 }
