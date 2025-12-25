eb97b082db51a8c82d62a734cdc049789e9eddc6
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-25 21:08:40 +0700

    Annotate `ServiceRegistryApplicationTest` with `@SpringBootTest` and set `test` profile properties

--- self_registration/services_registry/src/test/java/nawaphon/microservices/self_registration/service_registry/ServiceRegistryApplicationTest.java
+++ self_registration/services_registry/src/test/java/nawaphon/microservices/self_registration/service_registry/ServiceRegistryApplicationTest.java
@@ -1,8 +1,10 @@
 package nawaphon.microservices.self_registration.service_registry;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@SpringBootTest(properties = {"spring.profiles.active=test"})
 class ServiceRegistryApplicationTest {
 
 }
