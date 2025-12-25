6f637769c7519565e2cf7a8409e7db51c759ed59
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-25 21:15:08 +0700

    Inject `ApplicationContext` into `ServiceAApplicationTest` and add constructor

--- server_side_discovery/serviceA/src/test/java/nawaphon/microservices/server_side_discovery/service_a/ServiceAApplicationTest.java
+++ server_side_discovery/serviceA/src/test/java/nawaphon/microservices/server_side_discovery/service_a/ServiceAApplicationTest.java
@@ -2,12 +2,19 @@ package nawaphon.microservices.server_side_discovery.service_a;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class ServiceAApplicationTest {
+
+    private final ApplicationContext context;
+
+    ServiceAApplicationTest(ApplicationContext context) {
+        this.context = context;
+    }
 
     @Test
     void contextLoads() {
