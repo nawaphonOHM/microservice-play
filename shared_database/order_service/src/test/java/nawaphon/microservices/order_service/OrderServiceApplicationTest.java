84d8391cd5ad6f1c17d81f1b37e6c7ef27347037
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-25 21:20:06 +0700

    Inject `ApplicationContext` into `OrderServiceApplicationTest` via constructor

--- shared_database/order_service/src/test/java/nawaphon/microservices/order_service/OrderServiceApplicationTest.java
+++ shared_database/order_service/src/test/java/nawaphon/microservices/order_service/OrderServiceApplicationTest.java
@@ -1,10 +1,16 @@
 package nawaphon.microservices.order_service;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class OrderServiceApplicationTest {
 
+    private final ApplicationContext context;
+
+    OrderServiceApplicationTest(ApplicationContext context) {
+        this.context = context;
+    }
 }
