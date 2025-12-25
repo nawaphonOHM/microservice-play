947790e686b78846ad46056930feb025eb0509a5
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-25 20:58:52 +0700

    Annotate `MessagingRestSenderApplicationTest` with `@SpringBootTest` and set `test` profile properties

--- messaging/rest/sender/src/test/java/nawaphon/microservices/messaging/rest/MessagingRestSenderApplicationTest.java
+++ messaging/rest/sender/src/test/java/nawaphon/microservices/messaging/rest/MessagingRestSenderApplicationTest.java
@@ -1,8 +1,11 @@
 package nawaphon.microservices.messaging.rest;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
+@SpringBootTest(properties = {"spring.profiles.active=test"})
 class MessagingRestSenderApplicationTest {
 
 }
