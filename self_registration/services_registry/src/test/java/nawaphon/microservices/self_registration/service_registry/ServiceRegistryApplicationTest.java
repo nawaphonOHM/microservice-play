d4e97ee598510b3eea604fda5054ee43057ba87e
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-25 21:10:11 +0700

    Inject `ApplicationContext` into `ServiceRegistryApplicationTest` and add constructor

--- self_registration/services_registry/src/test/java/nawaphon/microservices/self_registration/service_registry/ServiceRegistryApplicationTest.java
+++ self_registration/services_registry/src/test/java/nawaphon/microservices/self_registration/service_registry/ServiceRegistryApplicationTest.java
@@ -2,11 +2,18 @@ package nawaphon.microservices.self_registration.service_registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class ServiceRegistryApplicationTest {
+
+    private final ApplicationContext context;
+
+    ServiceRegistryApplicationTest(ApplicationContext context) {
+        this.context = context;
+    }
 
     @Test
     void contextLoads() {
