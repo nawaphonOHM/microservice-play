9e4a8225ecb6a9d6a559fec57e0206a3fb17672c
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-12-25 21:09:45 +0700

    Add placeholder `contextLoads` test in `ServiceRegistryApplicationTest`

--- self_registration/services_registry/src/test/java/nawaphon/microservices/self_registration/service_registry/ServiceRegistryApplicationTest.java
+++ self_registration/services_registry/src/test/java/nawaphon/microservices/self_registration/service_registry/ServiceRegistryApplicationTest.java
@@ -7,4 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(properties = {"spring.profiles.active=test"})
 class ServiceRegistryApplicationTest {
 
+    void contextLoads() {
+    }
 }
