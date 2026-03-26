11403f4acdd986599c8d617fc580ea418090bc92
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-03-26 17:58:02 +0700

    Add API versioning to `/hello-world` endpoint and enable versioning configuration

--- client_side_discovery/client/src/main/java/nawaphon/microservices/client_side_discovery/client/controllers/MainController.java
+++ client_side_discovery/client/src/main/java/nawaphon/microservices/client_side_discovery/client/controllers/MainController.java
@@ -18,7 +18,7 @@ public class MainController {
         this.restTemplate = restTemplate;
     }
 
-    @GetMapping("/hello-world")
+    @GetMapping(value = "/hello-world", version = "1.0")
     public String helloWorld() {
         return this.restTemplate.getForObject("http://serviceA/client-side-discovery/service-a/hello-world", String.class);
     }
